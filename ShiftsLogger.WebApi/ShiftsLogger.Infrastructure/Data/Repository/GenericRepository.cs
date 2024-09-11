c4c4aedb3a1f3960cb5fe5d685335267a0264adc
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-09-11 00:30:03 -0600

    refactor: remove unused import in GenericRepository.cs

--- ShiftsLogger.WebApi/ShiftsLogger.Infrastructure/Data/Repository/GenericRepository.cs
+++ ShiftsLogger.WebApi/ShiftsLogger.Infrastructure/Data/Repository/GenericRepository.cs
@@ -2,7 +2,6 @@
 using Microsoft.EntityFrameworkCore;
 using ShiftsLogger.Application.Interfaces.Data.Repository;
 using ShiftsLogger.Application.Interfaces.Events;
-using ShiftsLogger.Domain.Events;
 using ShiftsLogger.Domain.Interfaces;
 
 namespace ShiftsLogger.Infrastructure.Data.Repository;
