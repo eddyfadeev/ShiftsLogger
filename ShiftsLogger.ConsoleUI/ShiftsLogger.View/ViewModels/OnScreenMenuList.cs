779973a07b08bfefa81df11a09c400fd456224d6
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-10-11 19:56:19 -0600

    feat(viewmodel): Refactor OnScreenMenuList class
    - Refactored logic for scrolling up and down in OnScreenMenuList class
    - Added UpdateVisibleElementsOnScroll method for updating visible elements
    - Fixed bug in GetVisibleElements method for calculating visible elements
    - Fixed ResizeService in Dispose method
    
    (dev)

--- ShiftsLogger.ConsoleUI/ShiftsLogger.View/ViewModels/OnScreenMenuList.cs
+++ ShiftsLogger.ConsoleUI/ShiftsLogger.View/ViewModels/OnScreenMenuList.cs
@@ -11,7 +11,6 @@ public class OnScreenMenuList<T> : IReturnsEntry<T>, IScrollable, IDisposable
     private readonly ObservableCollection<T> _allElements;
     private readonly object _lock = new();
     
-    private int _onScreenElementsCount;
     private int _offset;
     private bool _disposed = false;
 
@@ -22,7 +21,6 @@ public class OnScreenMenuList<T> : IReturnsEntry<T>, IScrollable, IDisposable
     {
         _allElements = new ObservableCollection<T>(menuEntries);
         VisibleElements = GetVisibleElements();
-        
         SubscribeToEvents();
     }
 
@@ -30,6 +28,14 @@ public class OnScreenMenuList<T> : IReturnsEntry<T>, IScrollable, IDisposable
     {
         lock (_lock)
         {
+            if (CurrentIndex < 0 || CurrentIndex >= VisibleElements.Count)
+            {
+                throw new InvalidOperationException(
+                    $"CurrentIndex: '{CurrentIndex}' is out of range. " +
+                    $"Number of elements in VisibleElements: {VisibleElements.Count}." +
+                    $"Number of elements in _allElements: {_allElements.Count}");
+            }
+            
             return VisibleElements.ElementAt(CurrentIndex);
         }
     }
@@ -38,19 +44,14 @@ public class OnScreenMenuList<T> : IReturnsEntry<T>, IScrollable, IDisposable
     {
         lock(_lock)
         {
-            if (CurrentIndex != 0)
+            if (CurrentIndex > 0)
             {
                 CurrentIndex--;
             }
-            else if (CurrentIndex == 0 && _offset > 0)
+            else if (_offset > 0)
             {
                 _offset--;
-                var lastElement = VisibleElements.Last;
-                if (lastElement != null)
-                {
-                    VisibleElements.RemoveLast();
-                    VisibleElements.AddFirst(_allElements[_offset]);
-                }
+                UpdateVisibleElementsOnScroll();
             }
         }
     }
@@ -63,15 +64,10 @@ public class OnScreenMenuList<T> : IReturnsEntry<T>, IScrollable, IDisposable
             {
                 CurrentIndex++;
             }
-            else if (CurrentIndex >= VisibleElements.Count - 1 && CurrentIndex + _offset < _allElements.Count - 1)
+            else if (CurrentIndex + _offset < _allElements.Count - 1)
             {
                 _offset++;
-                var firstElement = VisibleElements.First;
-                if (firstElement != null)
-                {
-                    VisibleElements.RemoveFirst();
-                    VisibleElements.AddLast(_allElements[_offset + _onScreenElementsCount - 1]);
-                }
+                UpdateVisibleElementsOnScroll();
             }
         }
     }
@@ -85,90 +81,44 @@ public class OnScreenMenuList<T> : IReturnsEntry<T>, IScrollable, IDisposable
             VisibleElements = GetVisibleElements();
         }
     }
-
-    private void OnElementHeightChanged()
-    {
-        lock (_lock)
-        {
-            VisibleElements = GetVisibleElements();
-        }
-    }
     
-    private void OnCollectionChanged(object? sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
-    {
-        lock (_lock)
-        {
-            AdjustForResize();
-            VisibleElements = GetVisibleElements();
-        }
-    }
-
-    private void OnConsoleResized()
-    {
-        lock(_lock)
-        {
-            int newOnScreenCount = CountOnScreenElements();
-
-            if (newOnScreenCount == _onScreenElementsCount)
-            {
-                return;
-            }
-            
-            _onScreenElementsCount = newOnScreenCount;
-            AdjustForResize();
-            VisibleElements = GetVisibleElements();
-        }
-    }
-
-    private void AdjustForResize()
-    {
-        _offset = Math.Max(0, Math.Min(_offset, _allElements.Count - _onScreenElementsCount));
-        
-        if (CurrentIndex >= _onScreenElementsCount)
-        {
-            _offset += (CurrentIndex - _onScreenElementsCount + 1);
-            CurrentIndex = _onScreenElementsCount - 1;
-        }
-        
-        CurrentIndex = Math.Min(CurrentIndex, _onScreenElementsCount - 1);
-    }
+    private void UpdateVisibleElementsOnScroll() =>
+        VisibleElements = GetVisibleElements();
     
     private LinkedList<T> GetVisibleElements()
     {
-        if (_allElements.Count == 0)
+        lock (_lock)
         {
-            return new LinkedList<T>();
+            int availableHeight = GetAvailableHeight();
+            var visibleElements = GetHeightAdjustedElements(availableHeight).ToList();
+            
+            _offset = Math.Max(0, Math.Min(_offset, _allElements.Count - visibleElements.Count));
+            
+            return new LinkedList<T>(visibleElements);
         }
-        _onScreenElementsCount = CountOnScreenElements();
-        
-        if (_allElements.Count <= _onScreenElementsCount)
-        {
-            return new LinkedList<T>(_allElements);
-        }
-        
-        _offset = Math.Max(0, Math.Min(_offset, _allElements.Count - _onScreenElementsCount));
-        
-        return new LinkedList<T>(_allElements.Skip(_offset).Take(_onScreenElementsCount));
     }
     
-    private int CountOnScreenElements()
+    private IEnumerable<T> GetHeightAdjustedElements(int availableHeight)
+    {
+        var elements = _allElements.Skip(_offset);
+        int height = 0;
+
+        foreach (var element in elements)
+        {
+            if (height + element.ElementHeight > availableHeight)
+            {
+                yield break;
+            }
+
+            yield return element;
+            height += element.ElementHeight;
+        }
+    }
+    
+    private static int GetAvailableHeight()
     {
         const int borders = 2; // sum of top and bottom border rows
-        int consoleWindowHeight = Console.WindowHeight;
-        int elementHeight = GetElementsHeight();
-
-        return (consoleWindowHeight - borders) / elementHeight;
-    }
-
-    private int GetElementsHeight()
-    {
-        if (!_allElements.Any())
-        {
-            return 1;
-        }
-        
-        var element = _allElements[0];
-        return element.ElementHeight;
+        return Console.WindowHeight - borders;
     }
 
     private void SubscribeToEvents()
@@ -187,7 +137,7 @@ public class OnScreenMenuList<T> : IReturnsEntry<T>, IScrollable, IDisposable
     {
         _allElements.CollectionChanged -= OnCollectionChanged;
         ResizeService.ConsoleResized -= OnConsoleResized;
-        ResizeService.Start();
+        ResizeService.Stop();
 
         foreach (var entity in _allElements)
         {
@@ -218,4 +168,14 @@ public class OnScreenMenuList<T> : IReturnsEntry<T>, IScrollable, IDisposable
     {
         Dispose(false);
     }
+
+    private void OnConsoleResized() =>
+        VisibleElements = GetVisibleElements();
+
+    private void OnElementHeightChanged() =>
+        VisibleElements = GetVisibleElements();
+
+    private void OnCollectionChanged(object? sender,
+        System.Collections.Specialized.NotifyCollectionChangedEventArgs e) =>
+        VisibleElements = GetVisibleElements();
 }
