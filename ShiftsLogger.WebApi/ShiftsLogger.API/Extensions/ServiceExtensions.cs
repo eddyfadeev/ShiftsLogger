e3a88bf58100062d4a17dfab3eb0ac9315376c59
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-08 15:18:41 -0700

    refactor: Remove unnecessary files and update service extensions in ServiceExtensions.cs.

---
+++ ShiftsLogger.WebApi/ShiftsLogger.API/Extensions/ServiceExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System.Reflection;
+using Contracts;
+using LoggerService;
+using Microsoft.OpenApi.Models;
+
+namespace ShiftsLogger.API.Extensions;
+
+public static class ServiceExtensions
+{
+    public static void ConfigureCors(this IServiceCollection services) =>
+        services.AddCors(options =>
+        {
+            options.AddPolicy("CorsPolicy", builder =>
+                builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+        });
+
+    public static void ConfigureIisIntegration(this IServiceCollection services) =>
+        services.Configure<IISOptions>(options =>
+        {
+
+        });
+    
+    public static void ConfigureSwagger(this IServiceCollection services) =>
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Version = "v1",
+                Title = "ShiftsLogger API",
+                Description = "A simple API to log worked shifts using ASP.NET Core Web API",
+            });
+
+            var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+            c.IncludeXmlComments(xmlPath);
+        });
+
+    public static void ConfigureLoggerService(this IServiceCollection services) =>
+        services.AddSingleton<ILoggerManager, LoggerManager>();
+}
