4551533ec6dd099c7edbdb383c7c0eabf7ff932b
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-08 22:17:48 -0700

    feat(repository): add LocationRepository class with CRUD operations for locations.

---
+++ ShiftsLogger.WebApi/Repository/LocationRepository.cs
@@ -0,0 +1,31 @@
+﻿using Contracts.Repository;
+using Entities.Models.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repository;
+
+public class LocationRepository : RepositoryBase<Location>, ILocationRepository
+{
+    public LocationRepository(RepositoryContext repositoryContext) : base(repositoryContext) {}
+
+    public async Task<IEnumerable<Location>> GetAllLocationsAsync(bool trackChanges) =>
+        await FindAll(trackChanges)
+            .OrderBy(l => l.Name)
+            .ToListAsync();
+
+    public async Task<Location?> GetLocationByIdAsync(Guid locationId, bool trackChanges) =>
+        await FindByCondition(l => l.Id.Equals(locationId), trackChanges)
+            .SingleOrDefaultAsync();
+
+    public void CreateLocation(Location location) =>
+        Create(location);
+
+    public void DeleteLocation(Location location) =>
+        Delete(location);
+
+    public void UpdateLocation(Location location) =>
+        Update(location);
+
+    public bool LocationExists(Guid locationId) =>
+        Exists(l => l.Id.Equals(locationId));
+}
