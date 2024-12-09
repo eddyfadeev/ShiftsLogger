30431c3e67b842ee8a3a8b76f4b1b4a58049bd59
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-08 22:18:11 -0700

    feat: Add LocationRepositoryTests class with setup, teardown, and test method for GetAllLocations method that returns ordered locations.

---
+++ ShiftsLogger.WebApi/Tests/Repository.Tests/LocationRepositoryTests.cs
@@ -0,0 +1,30 @@
+﻿using Entities.Models.Entity;
+
+namespace Repository.Tests;
+
+public class LocationRepositoryTests
+{
+    private RepositoryContext _context;
+    private LocationRepository _repository;
+
+    [SetUp]
+    public async Task Setup()
+    {
+        _context = await ImMemoryDbProvider.CreateDatabaseContext();
+        _repository = new LocationRepository(_context);
+    }
+
+    [TearDown]
+    public async Task TearDown()
+    {
+        await _context.DisposeAsync();
+    }
+
+    [Test]
+    public async Task GetAllLocations_ReturnsOrderedLocations()
+    {
+        var result = await _repository.GetAllLocationsAsync(trackChanges: false);
+        
+        Assert.That(result, Is.Ordered.By(nameof(Location.Name)));
+    }
+}
