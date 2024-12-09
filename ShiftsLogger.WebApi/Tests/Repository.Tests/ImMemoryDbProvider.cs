25d0e432cee1fb798275f6aeed27441621e0f626
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-08 22:17:58 -0700

    feat(tests): add ImMemoryDbProvider for creating in-memory database context in Repository.Tests
    - Added ImMemoryDbProvider class with CreateDatabaseContext method to generate in-memory database context
    - Ensures database is deleted and created before returning context
    - Utilizes GetInMemoryDbContext method to configure options and return RepositoryContext instance

---
+++ ShiftsLogger.WebApi/Tests/Repository.Tests/ImMemoryDbProvider.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Repository.Tests;
+
+internal static class ImMemoryDbProvider
+{
+    public static async Task<RepositoryContext> CreateDatabaseContext()
+    {
+        var context = GetInMemoryDbContext();
+
+        await context.Database.EnsureDeletedAsync();
+        await context.Database.EnsureCreatedAsync();
+
+        return context;
+    }
+
+    private static RepositoryContext GetInMemoryDbContext()
+    {
+        var options = new DbContextOptionsBuilder<RepositoryContext>()
+            .UseInMemoryDatabase(databaseName: $"TestDb_{Guid.NewGuid().ToString()}")
+            .Options;
+
+        return new RepositoryContext(options);
+    }
+}
