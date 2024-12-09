d8741feb7541705dafea9215118cb454895ee9ac
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-09 16:41:40 -0700

    feat(entity): add IEquatable implementation to Location entity class.

--- ShiftsLogger.WebApi/Entities/Models/Entity/Location.cs
+++ ShiftsLogger.WebApi/Entities/Models/Entity/Location.cs
@@ -4,7 +4,7 @@ using Entities.Interfaces;
 
 namespace Entities.Models.Entity;
 
-public class Location : IDbModel
+public sealed class Location : IDbModel, IEquatable<Location>
 {
     [Column("LocationId")]
     public Guid Id { get; init; }
@@ -17,4 +17,41 @@ public class Location : IDbModel
     public string? Address { get; init; }
     
     public ICollection<Shift>? Shifts { get; init; }
+
+
+    public bool Equals(Location? other)
+    {
+        if (other is null)
+        {
+            return false;
+        }
+
+        if (ReferenceEquals(this, other))
+        {
+            return true;
+        }
+        
+        return Id.Equals(other.Id) && 
+               Name == other.Name && 
+               Address == other.Address;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is null)
+        {
+            return false;
+        }
+
+        if (ReferenceEquals(this, obj))
+        {
+            return true;
+        }
+
+        return obj.GetType() == GetType() && 
+               Equals((Location)obj);
+    }
+
+    public override int GetHashCode() =>
+        HashCode.Combine(Id, Name, Address);
 }
