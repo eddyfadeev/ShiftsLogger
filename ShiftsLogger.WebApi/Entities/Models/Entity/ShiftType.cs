3134051fca38b6de8e121b31e2a963e994e35c5b
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-09 16:42:27 -0700

    refactor: Make ShiftType class sealed and implement IEquatable interface for equality comparison. Remove unnecessary virtual keyword from Shifts property

--- ShiftsLogger.WebApi/Entities/Models/Entity/ShiftType.cs
+++ ShiftsLogger.WebApi/Entities/Models/Entity/ShiftType.cs
@@ -4,7 +4,7 @@ using Entities.Interfaces;
 
 namespace Entities.Models.Entity;
 
-public class ShiftType : IDbModel
+public sealed class ShiftType : IDbModel, IEquatable<ShiftType>
 {
     [Column("ShiftTypeId")]
     public Guid Id { get; init; }
@@ -13,5 +13,42 @@ public class ShiftType : IDbModel
     [MaxLength(60, ErrorMessage = "Maximum length for the Name is 60 characters.")]
     public string? Name { get; init; }
     
-    public virtual ICollection<Shift>? Shifts { get; init; }
+    public ICollection<Shift>? Shifts { get; init; }
+
+    public bool Equals(ShiftType? other)
+    {
+        if (other is null)
+        {
+            return false;
+        }
+
+        if (ReferenceEquals(this, other))
+        {
+            return true;
+        }
+        
+        return Id.Equals(other.Id) && 
+               Name == other.Name;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is null)
+        {
+            return false;
+        }
+
+        if (ReferenceEquals(this, obj))
+        {
+            return true;
+        }
+        
+        return obj.GetType() == GetType() && 
+               Equals((ShiftType)obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Id, Name, Shifts);
+    }
 }
