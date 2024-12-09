5772d1c7a099d460c1405eb3b6a2c5c91ab35985
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-09 16:42:10 -0700

    refactor: Remove virtual keyword from Shifts property in User class, implement IEquatable<User>, override Equals and GetHashCode methods.

--- ShiftsLogger.WebApi/Entities/Models/Entity/User.cs
+++ ShiftsLogger.WebApi/Entities/Models/Entity/User.cs
@@ -4,7 +4,7 @@ using Entities.Interfaces;
 
 namespace Entities.Models.Entity;
 
-public class User : IDbModel
+public sealed class User : IDbModel, IEquatable<User>
 {
     [Column("UserId")]
     public Guid Id { get; init; }
@@ -24,5 +24,43 @@ public class User : IDbModel
     [MaxLength(60, ErrorMessage = "Maximum length for the Role is 60 characters.")]
     public string? Role { get; init; }
     
-    public virtual ICollection<Shift>? Shifts { get; init; }
+    public ICollection<Shift>? Shifts { get; init; }
+
+    public bool Equals(User? other)
+    {
+        if (other is null)
+        {
+            return false;
+        }
+        
+        if (ReferenceEquals(this, other))
+        {
+            return true;
+        }
+        
+        return Id.Equals(other.Id) && 
+               FirstName == other.FirstName && 
+               LastName == other.LastName && 
+               Email == other.Email && 
+               Role == other.Role;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is null)
+        {
+            return false;
+        }
+        
+        if (ReferenceEquals(this, obj))
+        {
+            return true;
+        }
+        
+        return obj.GetType() == GetType() && 
+               Equals((User)obj);
+    }
+
+    public override int GetHashCode() =>
+        HashCode.Combine(Id, FirstName, LastName, Email, Role);
 }
