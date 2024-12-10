1222ffb61fedb34e568629926545f266a48cae21
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-09 17:32:42 -0700

    feat(configuration): add LocationConfiguration for seeding default locations in the database.

---
+++ ShiftsLogger.WebApi/Repository/Configuration/LocationConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Entities.Models.Entity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Repository.Configuration;
+
+public class LocationConfiguration : IEntityTypeConfiguration<Location>
+{
+    public void Configure(EntityTypeBuilder<Location> builder)
+    {
+        builder.HasData(
+            new Location
+            {
+                Id = Guid.Parse(SeedConstants.Locations.LocationOneGuid),
+                Name = "Office",
+                Address = "777 Lucky Str. SW, Edmonton, AB, Canada"
+            },
+            new Location
+            {
+                Id = Guid.Parse(SeedConstants.Locations.LocationTwoGuid),
+                Name = "Home",
+                Address = "666 Sleepy Str. SW, Edmonton, AB, Canada"
+            }
+        );
+    }
+}
