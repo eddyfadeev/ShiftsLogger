fcce620f6e3fa372f83cd80af010874db63c58c8
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-09 18:46:30 -0700

    feat(api): Add IRepositoryManager interface with Location, User, ShiftType, Shift repositories and SaveAsync method.

---
+++ ShiftsLogger.WebApi/Contracts/IRepositoryManager.cs
@@ -0,0 +1,12 @@
+﻿using Contracts.Repository;
+
+namespace Contracts;
+
+public interface IRepositoryManager
+{
+    ILocationRepository Location { get; }
+    IUserRepository User { get; }
+    IShiftTypeRepository ShiftType { get; }
+    IShiftRepository Shift { get; }
+    Task SaveAsync();
+}
