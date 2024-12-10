9746d45aa46574e8617742f13469ae492700fe8d
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-09 21:05:42 -0700

    Create ShiftTypeNotFoundException class with a constructor that takes shiftTypeId parameter and throws an exception if shift type with that ID doesn't exist in the database.

---
+++ ShiftsLogger.WebApi/Entities/Exceptions/NotFound/ShiftTypeNotFoundException.cs
@@ -0,0 +1,4 @@
+﻿namespace Entities.Exceptions.NotFound;
+
+public class ShiftTypeNotFoundException(Guid shiftTypeId) 
+    : NotFoundException($"Shift type with ID: {shiftTypeId} doesn't exist in the database");
