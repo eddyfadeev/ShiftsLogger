a3ac3f56f21927239d06525ec0acd1df7883d3e7
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-09 17:50:47 -0700

    feat(repository): add ShiftTypeRepository class with CRUD methods for ShiftType entity.

---
+++ ShiftsLogger.WebApi/Repository/ShiftTypeRepository.cs
@@ -0,0 +1,31 @@
+﻿using Contracts.Repository;
+using Entities.Models.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repository;
+
+public class ShiftTypeRepository : RepositoryBase<ShiftType>, IShiftTypeRepository
+{
+    public ShiftTypeRepository(RepositoryContext repositoryContext) : base(repositoryContext) {}
+
+    public async Task<IEnumerable<ShiftType>> GetAllShiftTypesAsync(bool trackChanges) =>
+        await FindAll(trackChanges)
+            .OrderBy(st => st.Name)
+            .ToListAsync();
+
+    public async Task<ShiftType?> GetShiftTypeByIdAsync(Guid shiftTypeId, bool trackChanges) =>
+        await FindByCondition(st => st.Id.Equals(shiftTypeId), trackChanges)
+            .SingleOrDefaultAsync();
+
+    public void CreateShiftType(ShiftType shiftType) =>
+        Create(shiftType);
+
+    public void DeleteShiftType(ShiftType shiftType) =>
+        Delete(shiftType);
+
+    public void UpdateShiftType(ShiftType shiftType) =>
+        Update(shiftType);
+
+    public bool ShiftTypeExists(Guid shiftTypeId) =>
+        Exists(st => st.Id.Equals(shiftTypeId));
+}
