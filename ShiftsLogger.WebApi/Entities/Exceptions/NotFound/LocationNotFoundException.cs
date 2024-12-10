006a3fec65401deed0de66d3fbb31d7d7bc9295b
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-09 21:05:28 -0700

    feat: Add LocationNotFoundException class to handle cases where location with specific ID doesn't exist in the database.

---
+++ ShiftsLogger.WebApi/Entities/Exceptions/NotFound/LocationNotFoundException.cs
@@ -0,0 +1,4 @@
+﻿namespace Entities.Exceptions.NotFound;
+
+public class LocationNotFoundException(Guid locationId) 
+    : NotFoundException($"Location with ID: {locationId} doesn't exist in the database");
