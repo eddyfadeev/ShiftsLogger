f42f93b68f1b3f88ee9e281e89ccee09b4cf4eaa
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-09 19:25:04 -0700

    fix: Remove unused ExceptionHandlerMiddleware and add GlobalExceptionHandler to ConfigureServices in Startup.cs.

--- ShiftsLogger.WebApi/ShiftsLogger.API/Startup.cs
+++ ShiftsLogger.WebApi/ShiftsLogger.API/Startup.cs
@@ -1,7 +1,6 @@
 ﻿using System.Text.Json.Serialization;
 using Microsoft.AspNetCore.HttpOverrides;
 using ShiftsLogger.API.Extensions;
-using ShiftsLogger.API.Middleware;
 
 namespace ShiftsLogger.API;
 
@@ -20,6 +19,7 @@ public class Startup
         services.ConfigureRepositoryManager();
         services.ConfigureServiceManager();
         services.ConfigureSqlContext(_configuration);
+        services.AddExceptionHandler<GlobalExceptionHandler>();
         
         services.AddControllers()
             .AddJsonOptions(options => 
@@ -33,6 +33,7 @@ public class Startup
 
     public static void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
+        app.UseExceptionHandler(opt => { });
         app.UseSwagger();
         app.UseSwaggerUI(c =>
         {
@@ -48,8 +49,6 @@ public class Startup
         {
             app.UseHsts();
         }
-
-        app.UseMiddleware<ExceptionHandlerMiddleware>();
         
         app.UseRouting();
 
