e5f513bc7762937d265929441838e198b6738be4
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-09 17:50:05 -0700

    feat(repository): add UserRepository interface with CRUD operations for User entities.

---
+++ ShiftsLogger.WebApi/Contracts/Repository/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using Entities.Models.Entity;
+
+namespace Contracts.Repository;
+
+public interface IUserRepository
+{
+    Task<IEnumerable<User>> GetAllUsersAsync(bool trackChanges);
+    Task<User?> GetUserByIdAsync(Guid userId, bool trackChanges);
+    void CreateUser(User user);
+    void DeleteUser(User user);
+    void UpdateUser(User user);
+    bool UserExists(Guid userId);
+}
