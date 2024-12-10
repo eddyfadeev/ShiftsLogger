910341185bdeefb1d52a87b63058ddd176bd96e8
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-09 18:05:20 -0700

    feat(contract): Add IShiftRepository contract with methods for getting, creating, updating, and deleting shifts.

---
+++ ShiftsLogger.WebApi/Contracts/Repository/IShiftRepository.cs
@@ -0,0 +1,14 @@
+﻿using Entities.Models.Entity;
+
+namespace Contracts.Repository;
+
+public interface IShiftRepository
+{
+    Task<IEnumerable<Shift>> GetAllShiftsAsync(bool trackChanges);
+    Task<Shift?> GetShiftByIdAsync(Guid shiftId, bool trackChanges);
+    void CreateShift(Shift shift);
+    Task<IEnumerable<Shift>> GetByIdsAsync(IEnumerable<Guid> ids, bool trackChanges);
+    void DeleteShift(Shift shift);
+    void UpdateShift(Shift shift); 
+    bool ShiftExists(Guid shiftId);
+}
