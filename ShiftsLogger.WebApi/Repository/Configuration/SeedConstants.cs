485548b3927f46d04699cc57862c4d2acc623314
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-09 17:33:29 -0700

    feat(configuration): add constants for users, locations, and shift types in SeedConstants file.

---
+++ ShiftsLogger.WebApi/Repository/Configuration/SeedConstants.cs
@@ -0,0 +1,22 @@
+﻿namespace Repository.Configuration;
+
+internal static class SeedConstants
+{
+    public static class Users
+    {
+        public const string UserOneGuid = "487081EB-CE7A-45A9-8112-B0A563310E9D";
+        public const string UserTwoGuid = "CBE1F697-611C-431D-8503-E62D78615184";
+    }
+    
+    public static class Locations
+    {
+        public const string LocationOneGuid = "D7367CE4-91AA-483C-BA09-89CA48D97259";
+        public const string LocationTwoGuid = "4524CE96-D845-4F38-8699-4D5472285BC2";
+    }
+    
+    public static class ShiftTypes
+    {
+        public const string ShiftTypeOneGuid = "4B4F9AFE-B143-48BE-8C1F-B72B2FDDBDAB";
+        public const string ShiftTypeTwoGuid = "9D670142-F75F-40DB-9F96-2E7913B8ED05";
+    }
+}
