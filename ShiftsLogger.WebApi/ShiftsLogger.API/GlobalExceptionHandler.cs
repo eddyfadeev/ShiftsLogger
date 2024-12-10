1632fa1a4228e3fb2286252d5aec914d4cabd43c
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-09 19:24:56 -0700

    feat(api): add GlobalExceptionHandler class to handle exceptions in API responses

---
+++ ShiftsLogger.WebApi/ShiftsLogger.API/GlobalExceptionHandler.cs
@@ -0,0 +1,39 @@
+﻿using Contracts;
+using Entities.ErrorModel;
+using Microsoft.AspNetCore.Diagnostics;
+
+namespace ShiftsLogger.API;
+
+public class GlobalExceptionHandler : IExceptionHandler
+{
+    private readonly ILoggerManager _logger;
+
+    public GlobalExceptionHandler(ILoggerManager logger)
+    {
+        _logger = logger;
+    }
+    
+    public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+    {
+        httpContext.Response.ContentType = "application/json";
+
+        var contextFeature = httpContext.Features.Get<IExceptionHandlerFeature>();
+        if (contextFeature is not null)
+        {
+            httpContext.Response.StatusCode = contextFeature.Error switch
+            {
+                _ => StatusCodes.Status500InternalServerError
+            };
+            
+            _logger.LogError($"Something went wrong: { exception.Message }");
+            
+            await httpContext.Response.WriteAsync(new ErrorDetails()
+            {
+                StatusCode = httpContext.Response.StatusCode,
+                Message = contextFeature.Error.Message
+            }.ToString(), cancellationToken);
+        }
+
+        return true;
+    }
+}
