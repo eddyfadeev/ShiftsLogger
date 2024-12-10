bdff18aab941e9970594543f127981d572bb7d41
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-09 18:46:45 -0700

    feat(repository): add RepositoryManager class with lazy-loaded repository instances for location, user, shift type, and shift
    - Added RepositoryManager class implementing IRepositoryManager interface
    - Utilizes lazy loading for ILocationRepository, IUserRepository, IShiftTypeRepository, and IShiftRepository instances
    - Implements SaveAsync method for saving changes to the database

---
+++ ShiftsLogger.WebApi/Repository/RepositoryManger.cs
@@ -0,0 +1,43 @@
+﻿using Contracts;
+using Contracts.Repository;
+
+namespace Repository;
+
+public sealed class RepositoryManger : IRepositoryManager
+{
+    private readonly RepositoryContext _repositoryContext;
+    
+    private readonly Lazy<ILocationRepository> _locationRepository;
+    private readonly Lazy<IUserRepository> _userRepository;
+    private readonly Lazy<IShiftTypeRepository> _shiftTypeRepository;
+    private readonly Lazy<IShiftRepository> _shiftRepository;
+
+    public RepositoryManger(RepositoryContext repositoryContext)
+    {
+        _repositoryContext = repositoryContext;
+        _locationRepository = new Lazy<ILocationRepository>(() => 
+            new LocationRepository(repositoryContext));
+        _userRepository = new Lazy<IUserRepository>(() =>
+            new UserRepository(repositoryContext));
+        _shiftTypeRepository = new Lazy<IShiftTypeRepository>(() =>
+            new ShiftTypeRepository(repositoryContext));
+        _shiftRepository = new Lazy<IShiftRepository>(() =>
+            new ShiftRepository(repositoryContext));
+    }
+
+
+    public ILocationRepository Location => 
+        _locationRepository.Value;
+
+    public IUserRepository User =>
+        _userRepository.Value;
+
+    public IShiftTypeRepository ShiftType =>
+        _shiftTypeRepository.Value;
+
+    public IShiftRepository Shift =>
+        _shiftRepository.Value;
+
+    public async Task SaveAsync() =>
+        await _repositoryContext.SaveChangesAsync();
+}
