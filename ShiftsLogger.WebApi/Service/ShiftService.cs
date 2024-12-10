73fd325e77963170dc1a9595e466b34286634ad4
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-09 19:02:45 -0700

    feat(service): add ShiftService class with constructor for IRepositoryManager and ILoggerManager dependencies.

---
+++ ShiftsLogger.WebApi/Service/ShiftService.cs
@@ -0,0 +1,16 @@
+﻿using Contracts;
+using Service.Contracts;
+
+namespace Service;
+
+internal sealed class ShiftService : IShiftService
+{
+    private readonly IRepositoryManager _repository;
+    private readonly ILoggerManager _logger;
+
+    public ShiftService(IRepositoryManager repository, ILoggerManager logger)
+    {
+        _repository = repository;
+        _logger = logger;
+    }
+}
