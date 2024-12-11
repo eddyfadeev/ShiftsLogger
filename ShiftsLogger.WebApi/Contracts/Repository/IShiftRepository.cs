e47ccb85767a3ab7bf4e78f1c5688f3ca502a2ba
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-10 22:48:52 -0700

    feat(api): add method to get shifts for a specific location in IShiftRepository
    Adds a new method to IShiftRepository interface to retrieve shifts for a specific location based on locationId.

--- ShiftsLogger.WebApi/Contracts/Repository/IShiftRepository.cs
+++ ShiftsLogger.WebApi/Contracts/Repository/IShiftRepository.cs
@@ -6,6 +6,8 @@ namespace Contracts.Repository;
 public interface IShiftRepository
 {
     Task<PagedList<Shift>> GetAllShiftsAsync(ShiftParameters queryParameters, bool trackChanges);
+
+    Task<PagedList<Shift>> GetShiftsForLocation(Guid locationId, ShiftParameters queryParameters, bool trackChanges);
     Task<Shift?> GetShiftByIdAsync(Guid shiftId, bool trackChanges);
     void CreateShift(Shift shift);
     Task<IEnumerable<Shift>> GetByIdsAsync(IEnumerable<Guid> ids, bool trackChanges);
