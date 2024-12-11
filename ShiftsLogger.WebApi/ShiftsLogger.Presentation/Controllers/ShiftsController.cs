7b66b59c35d2c508520a0764eb3bdc4a4a2817e3
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-10 21:00:10 -0700

    feat(controller): add ShiftsController with GetShifts method for retrieving shifts from the database. Include routing for different scenarios and pagination headers.

---
+++ ShiftsLogger.WebApi/ShiftsLogger.Presentation/Controllers/ShiftsController.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+using Service.Contracts;
+using Shared.RequestFeatures;
+
+namespace ShiftsLogger.Presentation.Controllers;
+
+[Route("api/shifts")]
+[Route("api/locations/{locationId:guid}/shifts")]
+[Route("api/shift-types/{shiftTypeId:guid}/shifts")]
+[Route("api/users/{userId:guid}/shifts")]
+[ApiController]
+public class ShiftsController : ControllerBase
+{
+    private readonly IServiceManager _service;
+
+    public ShiftsController(IServiceManager service) =>
+        _service = service;
+
+    [HttpGet]
+    public async Task<IActionResult> GetShifts([FromQuery] ShiftParameters requestParameters)
+    {
+        var pagedResult = await _service.ShiftService.GetAllShiftsAsync(requestParameters, trackChanges: false);
+
+        if (pagedResult.shifts.Count <= 0)
+        {
+            return NoContent();
+        }
+        
+        Response.Headers["X-Pagination"] = JsonSerializer.Serialize(pagedResult.metaData);
+        
+        return Ok(pagedResult.shifts);
+    }
+
+}
