b37ca5068ac8fb9f9890d2abb1abb83472f1f964
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-10 21:15:01 -0700

    feat: Add DataContract attribute and DataMember attributes to ShiftDto properties to enable ordered serialization and deserialization.

--- ShiftsLogger.WebApi/Shared/Dto/ShiftDto.cs
+++ ShiftsLogger.WebApi/Shared/Dto/ShiftDto.cs
@@ -1,14 +1,21 @@
-﻿namespace Shared.Dto;
+﻿using System.Runtime.Serialization;
 
+namespace Shared.Dto;
+
+[DataContract]
 public record ShiftDto
 {
-    public Guid Id { get; init; }
-    public DateTime StartTime { get; init; }
-    public DateTime EndTime { get; init; }
-
-    public decimal HoursWorked { get; init; }
-
     private readonly string? _description;
+    
+    [DataMember(Order = 1)]
+    public Guid Id { get; init; }
+    [DataMember(Order = 2)]
+    public DateTime StartTime { get; init; }
+    [DataMember(Order = 3)]
+    public DateTime EndTime { get; init; }
+    [DataMember(Order = 4)]
+    public decimal HoursWorked { get; init; }
+    [DataMember(Order = 5)]
     public string? Description 
     { 
         get => _description;
