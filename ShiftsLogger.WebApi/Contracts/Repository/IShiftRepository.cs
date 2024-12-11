3b403009d23fe4ccec379bf07bb8627bfe60da3d
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-10 22:16:09 -0700

    fix: Correct parameter name in GetAllShiftsAsync method in IShiftRepository.cs to queryParameters to reflect actual usage.

--- ShiftsLogger.WebApi/Contracts/Repository/IShiftRepository.cs
+++ ShiftsLogger.WebApi/Contracts/Repository/IShiftRepository.cs
@@ -5,7 +5,7 @@ namespace Contracts.Repository;
 
 public interface IShiftRepository
 {
-    Task<PagedList<Shift>> GetAllShiftsAsync(ShiftParameters requestParameters, bool trackChanges);
+    Task<PagedList<Shift>> GetAllShiftsAsync(ShiftParameters queryParameters, bool trackChanges);
     Task<Shift?> GetShiftByIdAsync(Guid shiftId, bool trackChanges);
     void CreateShift(Shift shift);
     Task<IEnumerable<Shift>> GetByIdsAsync(IEnumerable<Guid> ids, bool trackChanges);
