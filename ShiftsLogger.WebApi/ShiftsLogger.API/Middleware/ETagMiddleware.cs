98a44006c5f054248e09ff7ca5f8a284a877b900
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-11 15:52:03 -0700

    feat(api): add ETagMiddleware to handle ETag generation for GET requests with status code 200.

---
+++ ShiftsLogger.WebApi/ShiftsLogger.API/Middleware/ETagMiddleware.cs
@@ -0,0 +1,26 @@
+﻿namespace ShiftsLogger.API.Middleware;
+
+public class ETagMiddleware
+{
+    private readonly RequestDelegate _next;
+    
+    public ETagMiddleware(RequestDelegate next) =>
+        _next = next;
+    
+    public async Task InvokeAsync(HttpContext context)
+    {
+        context.Response.OnStarting(() =>
+        {
+            if (context.Request.Method.Equals("GET") && 
+                context.Response.StatusCode == 200 )
+            {
+                var etag = $"\"{Guid.NewGuid():n}\"";
+                context.Response.Headers.ETag = etag;
+            }
+            
+            return Task.CompletedTask;
+        });
+        
+        await _next(context);
+    }
+}
