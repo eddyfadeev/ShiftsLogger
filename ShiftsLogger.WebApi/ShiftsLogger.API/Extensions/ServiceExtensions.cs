63308257c79cc0d0dd4d28f28005055c8e3d5df3
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-10 20:58:50 -0700

    feat(api): add exposed headers to CORS policy in ServiceExtensions.cs.

--- ShiftsLogger.WebApi/ShiftsLogger.API/Extensions/ServiceExtensions.cs
+++ ShiftsLogger.WebApi/ShiftsLogger.API/Extensions/ServiceExtensions.cs
@@ -17,7 +17,8 @@ public static class ServiceExtensions
             options.AddPolicy("CorsPolicy", builder =>
                 builder.AllowAnyOrigin()
                     .AllowAnyMethod()
-                    .AllowAnyHeader());
+                    .AllowAnyHeader()
+                    .WithExposedHeaders("X-Pagination"));
         });
 
     public static void ConfigureIisIntegration(this IServiceCollection services) =>
