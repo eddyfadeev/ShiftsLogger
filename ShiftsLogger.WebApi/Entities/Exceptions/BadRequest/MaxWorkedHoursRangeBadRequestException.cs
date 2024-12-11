8551e722cadb8a62214d5d98c2aba5c95a423fae
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-10 20:56:01 -0700

    feat(repository): Add MaxWorkedHoursRangeBadRequestException class to handle cases where Max Worked Hours is less than Min Worked Hours in shift configuration.

---
+++ ShiftsLogger.WebApi/Entities/Exceptions/BadRequest/MaxWorkedHoursRangeBadRequestException.cs
@@ -0,0 +1,4 @@
+﻿namespace Entities.Exceptions.BadRequest;
+
+public class MaxWorkedHoursRangeBadRequestException() 
+    : BadRequestException("Max Worked Hours cannot be less than Min Worked Hours");
