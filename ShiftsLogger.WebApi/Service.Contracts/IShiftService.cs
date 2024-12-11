0d4d4bdfd70280489e24d5383ae0b2f08444a28e
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-10 22:16:36 -0700

    feat(service): add method to retrieve shifts for a specific location in IShiftService interface

--- ShiftsLogger.WebApi/Service.Contracts/IShiftService.cs
+++ ShiftsLogger.WebApi/Service.Contracts/IShiftService.cs
@@ -6,4 +6,7 @@ namespace Service.Contracts;
 public interface IShiftService
 {
     Task<(List<ShiftDto> shifts, MetaData metaData)> GetAllShiftsAsync(ShiftParameters requestParameters, bool trackChanges);
+
+    Task<(List<ShiftDto> shifts, MetaData metaData)> GetShiftsForLocation(Guid locationId,
+        ShiftParameters requestParameters, bool trackChanges);
 }
