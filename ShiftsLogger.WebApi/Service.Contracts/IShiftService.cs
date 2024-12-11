7aeef9f592bbebbb6c6ab3e526c549a5d12ccf56
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-10 20:56:57 -0700

    feat(service): Add method to get all shifts asynchronously with request parameters and tracking changes.

--- ShiftsLogger.WebApi/Service.Contracts/IShiftService.cs
+++ ShiftsLogger.WebApi/Service.Contracts/IShiftService.cs
@@ -1,5 +1,9 @@
-﻿namespace Service.Contracts;
+﻿using Shared.Dto;
+using Shared.RequestFeatures;
+
+namespace Service.Contracts;
 
 public interface IShiftService
 {
+    Task<(List<ShiftDto> shifts, MetaData metaData)> GetAllShiftsAsync(ShiftParameters requestParameters, bool trackChanges);
 }
