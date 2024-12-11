cdc0f0092f388d58c76e0d1699c251357e2ff021
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-10 22:02:14 -0700

    feat(api): add output caching configuration in ServiceExtensions.cs. Add policy "15MinsExpiry" with 15 minutes expiry time using AddOutputCache method.

--- ShiftsLogger.WebApi/ShiftsLogger.API/Extensions/ServiceExtensions.cs
+++ ShiftsLogger.WebApi/ShiftsLogger.API/Extensions/ServiceExtensions.cs
@@ -54,4 +54,10 @@ public static class ServiceExtensions
     public static void ConfigureSqlContext(this IServiceCollection services, IConfiguration configuration) =>
         services.AddDbContext<RepositoryContext>(options =>
             options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+    public static void ConfigureOutputCaching(this IServiceCollection services) =>
+        services.AddOutputCache(options =>
+        {
+            options.AddPolicy("15MinsExpiry", p => p.Expire(TimeSpan.FromMinutes(15)));
+        });
 }
