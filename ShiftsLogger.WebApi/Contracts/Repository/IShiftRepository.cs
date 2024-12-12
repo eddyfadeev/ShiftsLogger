c007087771d6fda342bf5c406c35c3f485f54f72
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-11 22:37:22 -0700

    feat(repository): add methods to get shifts by shift type and user - Added two new methods to the IShiftRepository interface to allow retrieval of shifts based on shift type and user, using specified query parameters and tracking changes.

--- ShiftsLogger.WebApi/Contracts/Repository/IShiftRepository.cs
+++ ShiftsLogger.WebApi/Contracts/Repository/IShiftRepository.cs
@@ -7,6 +7,8 @@ public interface IShiftRepository
 {
     Task<PagedList<Shift>> GetAllShiftsAsync(ShiftParameters queryParameters, bool trackChanges);
     Task<PagedList<Shift>> GetShiftsForLocation(Guid locationId, ShiftParameters queryParameters, bool trackChanges);
+    Task<PagedList<Shift>> GetShiftsForShiftType(Guid shiftTypeId, ShiftParameters queryParameters, bool trackChanges);
+    Task<PagedList<Shift>> GetShiftsForUser(Guid userId, ShiftParameters queryParameters, bool trackChanges);
     Task<Shift?> GetShiftByIdAsync(Guid shiftId, bool trackChanges);
     void CreateShift(Shift shift);
     Task<IEnumerable<Shift>> GetByIdsAsync(IEnumerable<Guid> ids, bool trackChanges);
