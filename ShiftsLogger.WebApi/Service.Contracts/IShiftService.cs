9c4f31ef2acf1c6c2d12257a4a480ed1056c6746
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-11 22:37:03 -0700

    feat(service): add methods to retrieve shifts by location, shift type, and user in IShiftService interface.

--- ShiftsLogger.WebApi/Service.Contracts/IShiftService.cs
+++ ShiftsLogger.WebApi/Service.Contracts/IShiftService.cs
@@ -7,8 +7,15 @@ public interface IShiftService
 {
     Task<(List<ShiftDto> shifts, PaginationMetaData metaData)> 
         GetAllShiftsAsync(ShiftParameters requestParameters, bool trackChanges);
+    
     Task<(List<ShiftDto> shifts, PaginationMetaData metaData)> 
         GetShiftsForLocation(Guid locationId, ShiftParameters requestParameters, bool trackChanges);
+    
+    Task<(List<ShiftDto> shifts, PaginationMetaData metaData)> 
+        GetShiftsForShiftType(Guid shiftTypeId, ShiftParameters requestParameters, bool trackChanges);
+    
+    Task<(List<ShiftDto> shifts, PaginationMetaData metaData)> 
+        GetShiftsForUser(Guid userId, ShiftParameters requestParameters, bool trackChanges);
     
     Task<ShiftDto> GetShiftByIdAsync(Guid shiftId, bool trackChanges);
 }
