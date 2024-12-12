fdd6b5f29a306bd3382f5e3fdaaf94fd50df16a8
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-11 21:02:58 -0700

    feat(shifts): remove unnecessary empty line in IShiftRepository interface definition

--- ShiftsLogger.WebApi/Contracts/Repository/IShiftRepository.cs
+++ ShiftsLogger.WebApi/Contracts/Repository/IShiftRepository.cs
@@ -6,7 +6,6 @@ namespace Contracts.Repository;
 public interface IShiftRepository
 {
     Task<PagedList<Shift>> GetAllShiftsAsync(ShiftParameters queryParameters, bool trackChanges);
-
     Task<PagedList<Shift>> GetShiftsForLocation(Guid locationId, ShiftParameters queryParameters, bool trackChanges);
     Task<Shift?> GetShiftByIdAsync(Guid shiftId, bool trackChanges);
     void CreateShift(Shift shift);
