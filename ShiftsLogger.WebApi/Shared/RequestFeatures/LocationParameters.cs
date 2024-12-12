47a062ec1fd08b2f247b694752c64bcd800cb991
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-11 21:49:07 -0700

    refactor: Remove unnecessary FilterByName property in LocationParameters class search is enough.

--- ShiftsLogger.WebApi/Shared/RequestFeatures/LocationParameters.cs
+++ ShiftsLogger.WebApi/Shared/RequestFeatures/LocationParameters.cs
@@ -2,14 +2,6 @@
 
 public class LocationParameters : RequestParameters
 {
-    private string _filterByName = string.Empty;
-    
     public LocationParameters() =>
         OrderBy = "name asc";
-
-    public string FilterByName
-    {
-        get => _filterByName;
-        set => _filterByName = string.IsNullOrWhiteSpace(value) ? _filterByName : value;
-    }
 }
