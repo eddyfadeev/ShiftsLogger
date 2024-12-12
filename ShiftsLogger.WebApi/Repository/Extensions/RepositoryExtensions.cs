caf912e15c7cdffa5d706994f4b19f76060e0a37
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-11 21:03:49 -0700

    feat: Add RepositoryExtensions class with Page and Search methods for pagination and search functionality.

---
+++ ShiftsLogger.WebApi/Repository/Extensions/RepositoryExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq.Dynamic.Core;
+using Repository.Utility;
+using Shared.RequestFeatures;
+
+namespace Repository.Extensions;
+
+public static class RepositoryExtensions
+{
+    public static IQueryable<T> Page<T>(this IQueryable<T> queryable, RequestParameters queryParameters) =>
+        queryable
+            .Skip((queryParameters.PageNumber - 1) * queryParameters.PageSize)
+            .Take(queryParameters.PageSize);
+    
+    public static IQueryable<T> Search<T>(this IQueryable<T> query, RequestParameters queryParameters)
+    {
+        if (string.IsNullOrEmpty(queryParameters.Search))
+            return query;
+
+        var searchQuery = queryParameters.Search.Trim().ToLower();
+        var searchPredicate = QueryBuilder.CreateSearchQuery<T>(searchQuery);
+
+        return query.Where(searchPredicate, searchQuery);
+    }
+}
