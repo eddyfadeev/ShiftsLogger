38614cead077c4092a46783922281d057eef22ec
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-11 21:50:08 -0700

    feat(location): add method to retrieve all locations asynchronously with pagination metadata and query parameters.

--- ShiftsLogger.WebApi/Service.Contracts/ILocationService.cs
+++ ShiftsLogger.WebApi/Service.Contracts/ILocationService.cs
@@ -1,6 +1,10 @@
-﻿namespace Service.Contracts;
+﻿using Shared.Dto;
+using Shared.RequestFeatures;
+
+namespace Service.Contracts;
 
 public interface ILocationService
 {
-    
+    Task<(List<LocationDto> locations, PaginationMetaData metaData)> 
+        GetAllLocationsAsync(LocationParameters queryParameters, bool trackChanges);
 }
