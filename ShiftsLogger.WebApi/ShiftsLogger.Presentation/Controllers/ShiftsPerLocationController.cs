513ac857d86e57ec610e5ca67066d1403645695b
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-11 22:36:47 -0700

    feat(controller): add ShiftsPerLocationController for retrieving shifts by location. Set output caching for 15 minutes.

---
+++ ShiftsLogger.WebApi/ShiftsLogger.Presentation/Controllers/ShiftsPerLocationController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OutputCaching;
+using Service.Contracts;
+using Shared.RequestFeatures;
+using ShiftsLogger.Presentation.Extensions;
+
+namespace ShiftsLogger.Presentation.Controllers;
+
+[Route("api/locations/{locationId:guid}/shifts")]
+[ApiController]
+[OutputCache(PolicyName = "15MinsExpiry")]
+public class ShiftsPerLocationController : ControllerBase
+{
+    private readonly IServiceManager _service;
+    
+    public ShiftsPerLocationController(IServiceManager service) =>
+        _service = service;
+    
+    [HttpGet]
+    public async Task<IActionResult> GetShiftsForLocation(Guid locationId,
+        [FromQuery] ShiftParameters requestParameters)
+    {
+        var pagedResult =
+            await _service.ShiftService.GetShiftsForLocation(locationId, requestParameters, trackChanges: false);
+        
+        this.SetPaginationMetadata(pagedResult.metaData);
+        
+        return Ok(pagedResult.shifts);
+    }
+}
