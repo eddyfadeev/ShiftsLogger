e513a1a526660d785555d9946c6e1bc135b10084
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-11 21:22:57 -0700

    feat(shared): update default sorting in ShiftParameters constructor to sort by `start_time` in descending order.

--- ShiftsLogger.WebApi/Shared/RequestFeatures/ShiftParameters.cs
+++ ShiftsLogger.WebApi/Shared/RequestFeatures/ShiftParameters.cs
@@ -2,7 +2,7 @@
 
 public class ShiftParameters : RequestParameters
 {
-    public ShiftParameters() => OrderBy = "start_time";
+    public ShiftParameters() => OrderBy = "start_time desc";
     public DateTime? FromDate { get; set; } = DateTime.MinValue;
     public DateTime? ToDate { get; set; } = DateTime.MaxValue;
     public decimal MinWorkedHours { get; set; } = 0;
