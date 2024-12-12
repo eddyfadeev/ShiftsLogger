86c746cd779ee173673c73fdcebfc485c5013af9
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-11 22:36:03 -0700

    feat(api): create ShiftsPerUserController for retrieving shifts for a specific user by userId and query parameters. Set output caching for 15 minutes.

---
+++ ShiftsLogger.WebApi/ShiftsLogger.Presentation/Controllers/ShiftsPerUserController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OutputCaching;
+using Service.Contracts;
+using Shared.RequestFeatures;
+using ShiftsLogger.Presentation.Extensions;
+
+namespace ShiftsLogger.Presentation.Controllers;
+
+[Route("api/users/{userId:guid}/shifts")]
+[ApiController]
+[OutputCache(PolicyName = "15MinsExpiry")]
+public class ShiftsPerUserController : ControllerBase
+{
+    private readonly IServiceManager _service;
+    
+    public ShiftsPerUserController(IServiceManager service) =>
+        _service = service;
+    
+    [HttpGet]
+    public async Task<IActionResult> GetShiftsForUser(Guid userId,
+        [FromQuery] ShiftParameters requestParameters)
+    {
+        var pagedResult =
+            await _service.ShiftService.GetShiftsForUser(userId, requestParameters, trackChanges: false);
+        
+        this.SetPaginationMetadata(pagedResult.metaData);
+        
+        return Ok(pagedResult.shifts);
+    }
+}
