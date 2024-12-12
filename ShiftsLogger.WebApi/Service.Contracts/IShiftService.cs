220974215685cabd9bcc1360e58fe393b107f894
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-11 19:02:58 -0700

    feat(service): add method to get shift by id in IShiftService interface
    - Added a new method `GetShiftByIdAsync` in the `IShiftService` interface to retrieve a shift by its ID.

--- ShiftsLogger.WebApi/Service.Contracts/IShiftService.cs
+++ ShiftsLogger.WebApi/Service.Contracts/IShiftService.cs
@@ -6,7 +6,9 @@ namespace Service.Contracts;
 public interface IShiftService
 {
     Task<(List<ShiftDto> shifts, PaginationMetaData metaData)> GetAllShiftsAsync(ShiftParameters requestParameters, bool trackChanges);
-
+    // TODO: Consider to move it to the LocationService???
     Task<(List<ShiftDto> shifts, PaginationMetaData metaData)> GetShiftsForLocation(Guid locationId,
         ShiftParameters requestParameters, bool trackChanges);
+    
+    Task<ShiftDto> GetShiftByIdAsync(Guid shiftId, bool trackChanges);
 }
