0b386cc20ed6d35c964b5144318197c7861b633f
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-11 21:51:33 -0700

    feat(service): add method to get all locations with pagination metadata
    Added a new method `GetAllLocationsAsync` in `LocationService.cs` to retrieve all locations with pagination metadata by mapping and selecting DTOs from the retrieved locations.

--- ShiftsLogger.WebApi/Service/LocationService.cs
+++ ShiftsLogger.WebApi/Service/LocationService.cs
@@ -1,16 +1,26 @@
 ﻿using Contracts;
 using Service.Contracts;
+using Shared.Dto;
+using Shared.Mapper;
+using Shared.RequestFeatures;
 
 namespace Service;
 
 internal sealed class LocationService : ILocationService
 {
     private readonly IRepositoryManager _repository;
-    private readonly ILoggerManager _logger;
 
-    public LocationService(IRepositoryManager repository, ILoggerManager logger)
-    {
+    public LocationService(IRepositoryManager repository) =>
         _repository = repository;
-        _logger = logger;
+    
+
+    public async Task<(List<LocationDto> locations, PaginationMetaData metaData)> 
+        GetAllLocationsAsync(LocationParameters queryParameters, bool trackChanges)
+    {
+        var locations = await _repository.Location.GetAllLocationsAsync(queryParameters, trackChanges);
+
+        var dtos = locations.Select(l => l.MapToDto()).ToList();
+
+        return (dtos, locations.PaginationMetaData);
     }
 }
