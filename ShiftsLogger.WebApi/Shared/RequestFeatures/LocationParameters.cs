c254055630dad6ca5fb803cf3e23f95fd9e2d8ee
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-11 21:23:11 -0700

    feat: Add FilterByName property to LocationParameters class with default value of empty string, allowing filtering based on name in ascending order.

--- ShiftsLogger.WebApi/Shared/RequestFeatures/LocationParameters.cs
+++ ShiftsLogger.WebApi/Shared/RequestFeatures/LocationParameters.cs
@@ -2,6 +2,14 @@
 
 public class LocationParameters : RequestParameters
 {
+    private string _filterByName = string.Empty;
+    
     public LocationParameters() =>
-        OrderBy = "name";
+        OrderBy = "name asc";
+
+    public string FilterByName
+    {
+        get => _filterByName;
+        set => _filterByName = string.IsNullOrWhiteSpace(value) ? _filterByName : value;
+    }
 }
