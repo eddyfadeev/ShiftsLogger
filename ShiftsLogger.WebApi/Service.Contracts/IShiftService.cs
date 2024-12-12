cd28c3dfb2f0a9bd35a967a8a75c8551c84f629b
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-11 21:32:32 -0700

    refactor: removed TODO

--- ShiftsLogger.WebApi/Service.Contracts/IShiftService.cs
+++ ShiftsLogger.WebApi/Service.Contracts/IShiftService.cs
@@ -5,10 +5,10 @@ namespace Service.Contracts;
 
 public interface IShiftService
 {
-    Task<(List<ShiftDto> shifts, PaginationMetaData metaData)> GetAllShiftsAsync(ShiftParameters requestParameters, bool trackChanges);
-    // TODO: Consider to move it to the LocationService???
-    Task<(List<ShiftDto> shifts, PaginationMetaData metaData)> GetShiftsForLocation(Guid locationId,
-        ShiftParameters requestParameters, bool trackChanges);
+    Task<(List<ShiftDto> shifts, PaginationMetaData metaData)> 
+        GetAllShiftsAsync(ShiftParameters requestParameters, bool trackChanges);
+    Task<(List<ShiftDto> shifts, PaginationMetaData metaData)> 
+        GetShiftsForLocation(Guid locationId, ShiftParameters requestParameters, bool trackChanges);
     
     Task<ShiftDto> GetShiftByIdAsync(Guid shiftId, bool trackChanges);
 }
