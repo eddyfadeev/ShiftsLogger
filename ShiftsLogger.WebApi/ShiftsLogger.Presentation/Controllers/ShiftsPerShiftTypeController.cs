ad27accc9576b1b2b69bf92afd0b18f357f0191b
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-11 22:36:34 -0700

    feat: add ShiftsPerShiftTypeController with GetShiftsForShiftType method to retrieve shifts for a specific shift type. Set output caching for 15 minutes.

---
+++ ShiftsLogger.WebApi/ShiftsLogger.Presentation/Controllers/ShiftsPerShiftTypeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OutputCaching;
+using Service.Contracts;
+using Shared.RequestFeatures;
+using ShiftsLogger.Presentation.Extensions;
+
+namespace ShiftsLogger.Presentation.Controllers;
+
+[Route("api/shift-types/{shiftTypeId:guid}/shifts")]
+[ApiController]
+[OutputCache(PolicyName = "15MinsExpiry")]
+public class ShiftsPerShiftTypeController : ControllerBase
+{
+    private readonly IServiceManager _service;
+    
+    public ShiftsPerShiftTypeController(IServiceManager service) =>
+        _service = service;
+    
+    [HttpGet]
+    public async Task<IActionResult> GetShiftsForShiftType(Guid shiftTypeId,
+        [FromQuery] ShiftParameters requestParameters)
+    {
+        var pagedResult =
+            await _service.ShiftService.GetShiftsForShiftType(shiftTypeId, requestParameters, trackChanges: false);
+        
+        this.SetPaginationMetadata(pagedResult.metaData);
+        
+        return Ok(pagedResult.shifts);
+    }
+}
