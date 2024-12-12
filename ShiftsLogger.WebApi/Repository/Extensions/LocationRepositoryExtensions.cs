76e93479ebe7051aecf6fa53c12fb7d71175d972
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-11 21:23:18 -0700

    feat(repository): add LocationRepositoryExtensions for filtering and sorting locations based on query parameters.

---
+++ ShiftsLogger.WebApi/Repository/Extensions/LocationRepositoryExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq.Dynamic.Core;
+using Entities.Models;
+using Repository.Utility;
+using Shared.RequestFeatures;
+
+namespace Repository.Extensions;
+
+public static class LocationRepositoryExtensions
+{
+    public static IQueryable<Location>
+        Filter(this IQueryable<Location> locations, LocationParameters queryParameters) =>
+        locations.Where(l =>
+            l.Name!.Equals(queryParameters.FilterByName));
+
+    public static IQueryable<Location> Sort(this IQueryable<Location> locations, LocationParameters queryParameters)
+    {
+        if (string.IsNullOrWhiteSpace(queryParameters.OrderBy))
+        {
+            return locations.OrderBy(l => l.Name);
+        }
+        
+        var orderQuery = QueryBuilder.CreateOrderQuery<Location>(queryParameters.OrderBy);
+        
+        return string.IsNullOrWhiteSpace(orderQuery)        
+            ? locations.OrderBy(l => l.Name)         
+            : locations.OrderBy(orderQuery);
+    }   
+}
