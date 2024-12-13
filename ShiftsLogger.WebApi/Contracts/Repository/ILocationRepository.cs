2bb10e242117f71d9c4912d34c1855a26e83183d
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-13 15:01:01 -0700

    refactor: Remove unnecessary UpdateLocation method from ILocationRepository interface.

--- ShiftsLogger.WebApi/Contracts/Repository/ILocationRepository.cs
+++ ShiftsLogger.WebApi/Contracts/Repository/ILocationRepository.cs
@@ -9,6 +9,5 @@ public interface ILocationRepository
     Task<Location> GetLocationByIdAsync(Guid locationId, bool trackChanges);
     void CreateLocation(Location location);
     void DeleteLocation(Location location);
-    void UpdateLocation(Location location);
     bool LocationExists(Guid locationId);
 }
