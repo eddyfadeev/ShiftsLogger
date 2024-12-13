779c89d65d0d984f1aacdb0caab594560a7fb4ab
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-13 14:48:12 -0700

    feat: added CRUD operations for locations in LocationsController.cs

--- ShiftsLogger.WebApi/ShiftsLogger.Presentation/Controllers/LocationsController.cs
+++ ShiftsLogger.WebApi/ShiftsLogger.Presentation/Controllers/LocationsController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.OutputCaching;
 using Service.Contracts;
+using Shared.Dto.Location;
 using Shared.RequestFeatures;
+using ShiftsLogger.Presentation.ActionFilters;
 using ShiftsLogger.Presentation.Extensions;
 
 namespace ShiftsLogger.Presentation.Controllers;
@@ -20,9 +22,48 @@ public class LocationsController : ControllerBase
     public async Task<IActionResult> GetLocations([FromQuery] LocationParameters locationParameters)
     {
         var pagedResult = await _service.LocationService.GetAllLocationsAsync(locationParameters, trackChanges: false);
-        
+
         this.SetPaginationMetadata(pagedResult.metaData);
 
         return Ok(pagedResult.locations);
+    }
+
+    [HttpGet("{locationId:guid}", Name = "GetLocationById")]
+    public async Task<IActionResult> GetLocationById(Guid locationId)
+    {
+        var location = await _service.LocationService.GetLocationByIdAsync(locationId, trackChanges: false);
+
+        return Ok(location);
+    }
+
+    [HttpPost]
+    [ServiceFilter(typeof(ValidationFilterAttribute))]
+    public async Task<IActionResult> CreateLocation([FromBody] LocationForCreationDto? location)
+    {
+        var createdLocation = await _service.LocationService.CreateLocationAsync(location!);
+
+        return CreatedAtRoute
+        (
+            "GetLocationById",
+            new { id = createdLocation.Id },
+            createdLocation
+        );
+    }
+
+    [HttpDelete("{locationId:guid}")]
+    public async Task<IActionResult> DeleteLocation(Guid locationId)
+    {
+        await _service.LocationService.DeleteLocationAsync(locationId, trackChanges: false);
+
+        return NoContent();
+    }
+
+    [HttpPut("{locationId:guid}")]
+    [ServiceFilter(typeof(ValidationFilterAttribute))]
+    public async Task<IActionResult> UpdateLocation(Guid locationId, LocationForUpdateDto location)
+    {
+        await _service.LocationService.UpdateLocationAsync(locationId, location, trackChanges: true);
+
+        return NoContent();
     }
 }
