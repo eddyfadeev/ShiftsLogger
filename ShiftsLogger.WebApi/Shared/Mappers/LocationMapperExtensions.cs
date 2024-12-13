2e76e5f9a3c161ebef4d6dc0eeeb67380306746f
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-13 13:34:03 -0700

    feat(shared): add LocationMapperExtensions class for mapping location entities to DTOs and vice versa

---
+++ ShiftsLogger.WebApi/Shared/Mappers/LocationMapperExtensions.cs
@@ -0,0 +1,38 @@
+﻿using Entities.Models;
+using Shared.Dto.Location;
+
+namespace Shared.Mappers;
+
+public static class LocationMapperExtensions
+{
+    public static LocationDto MapToDto(this Location location) =>
+        new()
+        {
+            Id = location.Id,
+            Name = location.Name,
+            Address = location.Address ?? string.Empty
+        };
+
+    public static Location MapToEntity(this LocationForCreationDto location) =>
+        new()
+        {
+            Name = location.Name,
+            Address = location.Address,
+        };
+
+    public static Location MapToEntity(this LocationDto location) =>
+        new()
+        {
+            Id = location.Id,
+            Name = location.Name,
+            Address = location.Address
+        };
+    
+    public static Location UpdateEntity(this Location location, LocationForUpdateDto updateDto)
+    {
+        location.Name = string.IsNullOrWhiteSpace(updateDto.Name) ? location.Name : updateDto.Name;
+        location.Address= string.IsNullOrWhiteSpace(updateDto.Address) ? location.Address : updateDto.Address;
+        
+        return location;
+    }
+}
