5ea232c1d732b2487622ea809d5487374da584aa
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-13 19:57:20 -0700

    fix: change return type of GetLocationByIdAsync method to nullable Location to handle possible null return value

--- ShiftsLogger.WebApi/Contracts/Repository/ILocationRepository.cs
+++ ShiftsLogger.WebApi/Contracts/Repository/ILocationRepository.cs
@@ -6,7 +6,7 @@ namespace Contracts.Repository;
 public interface ILocationRepository
 {
     Task<PagedList<Location>> GetAllLocationsAsync(LocationParameters queryParameters, bool trackChanges);
-    Task<Location> GetLocationByIdAsync(Guid locationId, bool trackChanges);
+    Task<Location?> GetLocationByIdAsync(Guid locationId, bool trackChanges);
     void CreateLocation(Location location);
     void DeleteLocation(Location location);
     bool LocationExists(Guid locationId);
