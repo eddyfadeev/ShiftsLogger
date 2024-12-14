3aea837a05ffe9769e69af3c78646c75e235d6aa
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-13 22:35:14 -0700

    feat: Add default sorting by firstName in UserParameters class.

--- ShiftsLogger.WebApi/Shared/RequestFeatures/UserParameters.cs
+++ ShiftsLogger.WebApi/Shared/RequestFeatures/UserParameters.cs
@@ -1,3 +1,7 @@
 ﻿namespace Shared.RequestFeatures;
 
-public class UserParameters : RequestParameters;
+public class UserParameters : RequestParameters
+{
+    public UserParameters() =>
+        OrderBy = "firstName";
+}
