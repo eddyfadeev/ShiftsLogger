bd171c1bbea6f31dfd1862c3c66b5ea6521379d2
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-13 22:36:31 -0700

    fix: Remove unnecessary null check on shiftType in ShiftTypeService.cs
    Removed unnecessary null check on shiftType before mapping to DTO in GetShiftTypeByIdAsync method to improve code readability and efficiency.

--- ShiftsLogger.WebApi/Service/ShiftTypeService.cs
+++ ShiftsLogger.WebApi/Service/ShiftTypeService.cs
@@ -30,7 +30,7 @@ internal sealed class ShiftTypeService : IShiftTypeService
     {
         var shiftType = await TryGetShiftTypeEntity(shiftTypeId, trackChanges);
 
-        return shiftType!.MapToDto();
+        return shiftType.MapToDto();
     }
 
     public async Task<ShiftTypeDto> CreateShiftTypeAsync(ShiftTypeForCreationDto shiftType)
@@ -47,21 +47,21 @@ internal sealed class ShiftTypeService : IShiftTypeService
     {
         var entity = await TryGetShiftTypeEntity(shiftTypeId, trackChanges);
         
-        _repository.ShiftType.DeleteShiftType(entity!);
+        _repository.ShiftType.DeleteShiftType(entity);
         await _repository.SaveAsync();
     }
 
-    public async Task UpdateShiftTypeAsync(Guid shiftTypeId, ShiftTypeForUpdateDto shiftTypeForUpdate,
+    public async Task UpdateShiftTypeAsync(Guid shiftTypeId, ShiftTypeForUpdateDto updateDto,
         bool trackChanges)
     {
         var entity = await TryGetShiftTypeEntity(shiftTypeId, trackChanges);
         
-        entity?.UpdateEntity(shiftTypeForUpdate);
+        entity.UpdateEntity(updateDto);
         await _repository.SaveAsync();
     }
 
-    private async Task<ShiftType?> TryGetShiftTypeEntity(Guid shiftTypeId, bool trackChanges) =>
-        _repository.ShiftType.ShiftTypeExists(shiftTypeId)
+    private async Task<ShiftType> TryGetShiftTypeEntity(Guid shiftTypeId, bool trackChanges) =>
+        (_repository.ShiftType.ShiftTypeExists(shiftTypeId)
             ? await _repository.ShiftType.GetShiftTypeByIdAsync(shiftTypeId, trackChanges)
-            : throw new ShiftTypeNotFoundException(shiftTypeId);
+            : throw new ShiftTypeNotFoundException(shiftTypeId))!;
 }
