c48e10bba999320f7b1cb1bda0e0f31ea01cc22b
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-13 22:39:58 -0700

    feat: Add Sort method to UserRepositoryExtensions for sorting users based on query parameters. If no specific order is provided, sort by FirstName. If an order query is provided, sort according to that query.

---
+++ ShiftsLogger.WebApi/Repository/Extensions/UserRepositoryExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq.Dynamic.Core;
+using Entities.Models;
+using Repository.Utility;
+using Shared.RequestFeatures;
+
+namespace Repository.Extensions;
+
+public static class UserRepositoryExtensions
+{
+    public static IQueryable<User> Sort(this IQueryable<User> users, UserParameters queryParameters)
+    {
+        if (string.IsNullOrWhiteSpace(queryParameters.OrderBy))
+        {
+            return users.OrderBy(u => u.FirstName);
+        }
+        
+        var orderQuery = QueryBuilder.CreateOrderQuery<Location>(queryParameters.OrderBy);
+        
+        return string.IsNullOrWhiteSpace(orderQuery)        
+            ? users.OrderBy(u => u.FirstName)         
+            : users.OrderBy(orderQuery);
+    }
+}
