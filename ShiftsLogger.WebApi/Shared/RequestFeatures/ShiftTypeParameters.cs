5a3fe313abb25ab42a2b458db813c79e3bb0ca79
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-13 19:57:42 -0700

    fix: Add missing curly braces and default sorting order to ShiftTypeParameters class.

--- ShiftsLogger.WebApi/Shared/RequestFeatures/ShiftTypeParameters.cs
+++ ShiftsLogger.WebApi/Shared/RequestFeatures/ShiftTypeParameters.cs
@@ -1,3 +1,7 @@
 ﻿namespace Shared.RequestFeatures;
 
-public class ShiftTypeParameters : RequestParameters;
+public class ShiftTypeParameters : RequestParameters
+{
+    public ShiftTypeParameters() => 
+        OrderBy = "name asc";
+}
