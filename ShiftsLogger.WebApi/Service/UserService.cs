e0f2e9c56075da56cacaf570f67422ade92c654c
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-13 22:40:30 -0700

    refactor: Updated UserService class to include methods for getting, creating, updating, and deleting users, along with error handling for user not found.

--- ShiftsLogger.WebApi/Service/UserService.cs
+++ ShiftsLogger.WebApi/Service/UserService.cs
@@ -1,16 +1,65 @@
 ﻿using Contracts;
+using Entities.Exceptions.NotFound;
+using Entities.Models;
 using Service.Contracts;
+using Shared.Dto.User;
+using Shared.Mappers;
+using Shared.RequestFeatures;
 
 namespace Service;
 
 internal sealed class UserService : IUserService
 {
     private readonly IRepositoryManager _repository;
-    private readonly ILoggerManager _logger;
 
-    public UserService(IRepositoryManager repository, ILoggerManager logger)
-    {
+    public UserService(IRepositoryManager repository) =>
         _repository = repository;
-        _logger = logger;
+
+    public async Task<(List<UserDto> users, PaginationMetaData metaData)> 
+        GetAllUsersAsync(UserParameters userParameters, bool trackChanges)
+    {
+        var users = await _repository.User.GetAllUsersAsync(userParameters, trackChanges);
+
+        var dtos = users.Select(u => u.MapToDto()).ToList();
+
+        return (dtos, users.PaginationMetaData);
     }
+
+    public async Task<UserDto> GetUserByIdAsync(Guid userId, bool trackChanges)
+    {
+        var user = await TryGetUserEntity(userId, trackChanges);
+        
+        return user.MapToDto();
+    }
+
+    public async Task<UserDto> CreateUserAsync(UserForCreationDto user)
+    {
+        var entity = user.MapToEntity();
+        
+        _repository.User.CreateUser(entity);
+        await _repository.SaveAsync();
+
+        return entity.MapToDto();
+    }
+
+    public async Task DeleteUserAsync(Guid userId, bool trackChanges)
+    {
+        var entity = await TryGetUserEntity(userId, trackChanges);
+
+        _repository.User.DeleteUser(entity);
+        await _repository.SaveAsync();
+    }
+
+    public async Task UpdateUserAsync(Guid userId, UserForUpdateDto updateDto, bool trackChanges)
+    {
+        var entity  = await TryGetUserEntity(userId, trackChanges);
+
+        entity.UpdateEntity(updateDto);
+        await _repository.SaveAsync();
+    }
+
+    private async Task<User> TryGetUserEntity(Guid userId, bool trackChanges) =>
+        (_repository.User.UserExists(userId)
+            ? await _repository.User.GetUserByIdAsync(userId, trackChanges)
+            : throw new UserNotFoundException(userId))!;
 }
