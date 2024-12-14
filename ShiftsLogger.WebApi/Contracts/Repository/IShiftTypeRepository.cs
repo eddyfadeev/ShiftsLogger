9774f752cb55fdb47946f4ee8eb6992383cd6084
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-13 19:58:00 -0700

    Refactor ShiftTypeRepository to include pagination and filtering logic in GetAllShiftTypesAsync method.

--- ShiftsLogger.WebApi/Contracts/Repository/IShiftTypeRepository.cs
+++ ShiftsLogger.WebApi/Contracts/Repository/IShiftTypeRepository.cs
@@ -1,13 +1,13 @@
 ﻿using Entities.Models;
+using Shared.RequestFeatures;
 
 namespace Contracts.Repository;
 
 public interface IShiftTypeRepository
 {
-    Task<IEnumerable<ShiftType>> GetAllShiftTypesAsync(bool trackChanges);
+    Task<PagedList<ShiftType>> GetAllShiftTypesAsync(ShiftTypeParameters queryParameters, bool trackChanges);
     Task<ShiftType?> GetShiftTypeByIdAsync(Guid shiftTypeId, bool trackChanges);
     void CreateShiftType(ShiftType shiftType);
     void DeleteShiftType(ShiftType shiftType);
-    void UpdateShiftType(ShiftType shiftType);
     bool ShiftTypeExists(Guid shiftTypeId);
 }
