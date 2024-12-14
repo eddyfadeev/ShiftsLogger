3df6c95a4a12fd1436b462d537eee3283b777e81
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-14 16:48:01 -0700

    feat: Add ShiftForManipulationDto, ShiftForUpdateDto, ShiftDto, ShiftForCreationDto classes with necessary properties for Shift management.

--- ShiftsLogger.WebApi/Shared/Dto/Shift/ShiftDto.cs
+++ ShiftsLogger.WebApi/Shared/Dto/Shift/ShiftDto.cs
@@ -2,28 +2,11 @@
 
 namespace Shared.Dto.Shift;
 
-[DataContract]
-public record ShiftDto
+
+public record ShiftDto : ShiftForManipulationDto
 {
-    private readonly string? _description;
-    
     [DataMember(Order = 1)] 
     public Guid Id { get; init; }
     [DataMember(Order = 2)] 
     public required string User { get; init; }
-    [DataMember(Order = 3)] 
-    public required string Location { get; init; }
-    [DataMember(Order = 4)] 
-    public DateTime StartTime { get; init; }
-    [DataMember(Order = 5)] 
-    public DateTime EndTime { get; init; }
-    [DataMember(Order = 6)] 
-    public required string ShiftType { get; init; }
-    [DataMember(Order = 7)] 
-    public decimal HoursWorked { get; init; }
-    public string? Description 
-    { 
-        get => _description;
-        init => _description = value ?? string.Empty;
-    }
 }
