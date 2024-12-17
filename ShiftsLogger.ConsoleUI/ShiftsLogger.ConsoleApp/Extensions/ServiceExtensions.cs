cc2f6bffb7d538569e6ff5822e921d814791de63
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-16 20:59:23 -0700

    feat: Add ServiceExtensions class to handle API endpoint configuration

---
+++ ShiftsLogger.ConsoleUI/ShiftsLogger.ConsoleApp/Extensions/ServiceExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Entity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ShiftsLogger.ConsoleApp.Extensions;
+
+public static class ServiceExtensions
+{
+    public static void ConfigureApiEndpoints(this IServiceCollection services, IConfiguration configuration) =>
+        services.Configure<ApiEndpointsOptions>(configuration.GetSection(ApiEndpointsOptions.ApiEndpoints));
+}
