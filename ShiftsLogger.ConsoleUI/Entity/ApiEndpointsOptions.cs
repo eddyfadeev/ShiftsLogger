c6a2a1655f0daa9c1f697e84a03d525d2c44f34d
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-16 20:59:12 -0700

    Create Entity.csproj and ApiEndpointsOptions.cs files in the Entity folder with ApiEndpointsOptions.cs

---
+++ ShiftsLogger.ConsoleUI/Entity/ApiEndpointsOptions.cs
@@ -0,0 +1,13 @@
+﻿namespace Entity;
+
+public class ApiEndpointsOptions
+{
+    public const string ApiEndpoints = "ApiEndpoints";
+    
+    public required string BaseUrl { get; set; }
+    
+    public required string Shifts { get; set; }
+    public required string Users { get; set; }
+    public required string Locations { get; set; }
+    public required string ShiftTypes { get; set; }
+}
