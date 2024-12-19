39211a951009f56f4ed5fa1522a796ccb7c7bec6
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-19 16:48:58 -0700

    feat(contracts): Add Contracts.csproj, IHttpClientBase.cs, and IApiClient.cs interfaces for HttpClient and Api client functionality.

---
+++ ShiftsLogger.ConsoleUI/Contracts/IHttpClientBase.cs
@@ -0,0 +1,12 @@
+﻿using Shared.RequestFeatures;
+
+namespace Contracts;
+
+public interface IHttpClientBase<TEntity>
+{
+    Task<TEntity?> GetByIdAsync(Uri endpoint);
+    Task<PagedList<TEntity>?> GetAllAsync(Uri endpoint);
+    Task<TEntity?> CreateAsync(Uri endpoint, TEntity entity);
+    Task UpdateAsync(Uri endpoint, TEntity entity);
+    Task DeleteAsync(Uri uri);
+}
