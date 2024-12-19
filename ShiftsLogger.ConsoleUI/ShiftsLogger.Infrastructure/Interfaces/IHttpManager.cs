39bdb9b4bf87011ba2bdbbeccb84b7efce8c4838
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-19 16:47:20 -0700

    Deleted multiple files and their dependencies in the ShiftsLogger project.

--- ShiftsLogger.ConsoleUI/ShiftsLogger.Infrastructure/Interfaces/IHttpManager.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ShiftsLogger.Infrastructure.Interfaces;
-
-public interface IHttpManager
-{
-    internal HttpClient GetHttpClient();
-    internal void EnsureSuccessStatusCode(HttpResponseMessage response, Uri url);
-}
--- ShiftsLogger.ConsoleUI/ShiftsLogger.Infrastructure/Interfaces/IRequestHandler.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ShiftsLogger.Infrastructure.Interfaces;
-
-public interface IRequestHandler
-{
-    internal Task<string> GetAsync(Uri url);
-    internal Task<HttpResponseMessage> PostAsync(Uri url, HttpContent content);
-    internal Task<HttpResponseMessage?> DeleteAsync(Uri url);
-}
