7dbb86f97e3186cdf4adc0e5a30f92755a7ecc94
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-21 13:54:27 -0700

    feat(api): add ApiServiceManager class with lazy initialization of client services.

---
+++ ShiftsLogger.ConsoleUI/Service/ApiServiceManager.cs
@@ -0,0 +1,45 @@
+﻿using Contracts;
+using Entity;
+using Microsoft.Extensions.Options;
+using Service.Clients;
+using Service.Contracts;
+using Service.Contracts.Clients;
+
+namespace Service;
+
+public class ApiServiceManager : IApiServiceManager
+{
+    private readonly Lazy<IUserService> _userClient;
+    private readonly Lazy<IShiftTypeService> _shiftTypeClient;
+    private readonly Lazy<ILocationService> _locationClient;
+    private readonly Lazy<IShiftService> _shiftClient;
+
+    public ApiServiceManager(IHttpClientFactory factory, IOptions<ApiEndpointsOptions> options, ILoggerManager logger)
+    {
+        var httpClient = factory.CreateClient();
+        
+        _userClient = new Lazy<IUserService>(() => 
+            new UserClient(httpClient, options, logger));
+
+        _shiftTypeClient = new Lazy<IShiftTypeService>(() =>
+            new ShiftTypeClient(httpClient, options, logger));
+
+        _locationClient = new Lazy<ILocationService>(() =>
+            new LocationClient(httpClient, options, logger));
+
+        _shiftClient = new Lazy<IShiftService>(() =>
+            new ShiftClient(httpClient, options, logger));
+    }
+
+    public IUserService User =>
+        _userClient.Value;
+
+    public IShiftTypeService ShiftType =>
+        _shiftTypeClient.Value;
+
+    public ILocationService Location =>
+        _locationClient.Value;
+
+    public IShiftService Shift =>
+        _shiftClient.Value;
+}
