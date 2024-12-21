93caf90bca3cea66be97e4537ab8593ecb26c0dd
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-21 01:56:10 -0700

    fix: corrected return type in GetAllAsync method in IHttpClientBase interface

--- ShiftsLogger.ConsoleUI/Contracts/IHttpClientBase.cs
+++ ShiftsLogger.ConsoleUI/Contracts/IHttpClientBase.cs
@@ -5,8 +5,8 @@ namespace Contracts;
 public interface IHttpClientBase<TEntity>
 {
     Task<TEntity?> GetByIdAsync(Uri endpoint);
-    Task<PagedList<TEntity>?> GetAllAsync(Uri endpoint);
+    Task<PagedList<TEntity>> GetAllAsync(Uri endpoint);
     Task<TEntity?> CreateAsync(Uri endpoint, TEntity entity);
     Task UpdateAsync(Uri endpoint, TEntity entity);
-    Task DeleteAsync(Uri uri);
+    Task DeleteAsync(Uri endpoint);
 }
