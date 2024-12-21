350ee18616d10df4c7cde5c34b2426d2af922ecf
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-21 13:54:14 -0700

    feat(service): add IApiServiceManager interface with User, ShiftType, Location, and Shift services properties.

---
+++ ShiftsLogger.ConsoleUI/Service.Contracts/IApiServiceManager.cs
@@ -0,0 +1,14 @@
+﻿using Service.Contracts.Clients;
+
+namespace Service.Contracts;
+
+public interface IApiServiceManager
+{
+    IUserService User { get; }
+
+    IShiftTypeService ShiftType { get; }
+
+    ILocationService Location { get; }
+
+    IShiftService Shift { get; }
+}
