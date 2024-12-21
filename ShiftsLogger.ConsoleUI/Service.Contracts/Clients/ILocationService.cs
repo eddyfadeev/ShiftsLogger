c6426c8781c00ee59755fb81f08f783ac5e88562
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-21 13:52:42 -0700

    feat(service): Add ILocationService interface and LocationClient class
    Added a new interface `ILocationService` with methods to get, create, update, and delete locations. Also, implemented `LocationClient` class to handle API transactions for locations.

---
+++ ShiftsLogger.ConsoleUI/Service.Contracts/Clients/ILocationService.cs
@@ -0,0 +1,13 @@
+﻿using Shared.Dto.Location;
+using Shared.RequestFeatures;
+
+namespace Service.Contracts.Clients;
+
+public interface ILocationService
+{
+    Task<LocationDto?> GetLocationByIdAsync(Guid locationId);
+    Task<PagedList<LocationDto>> GetAllLocationsAsync(LocationParameters? requestParameters = null);
+    Task<LocationDto?> CreateLocationAsync(LocationDto location);
+    Task DeleteLocationAsync(Guid locationId);
+    Task UpdateLocationAsync(Guid locationId, LocationDto location);
+}
