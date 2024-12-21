1bf7312973a8ebc5c3b26aa6dbd2148d51f38a15
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-21 01:56:20 -0700

    refactor: Improvements in HttpClientBase.cs including new JsonSerializerOptions, logging, and error handling for different API calls.

--- ShiftsLogger.ConsoleUI/HttpManager/HttpClientBase.cs
+++ ShiftsLogger.ConsoleUI/HttpManager/HttpClientBase.cs
@@ -1,51 +1,138 @@
-﻿using System.Net.Http.Json;
+﻿using System.Net.Http.Headers;
+using System.Net.Http.Json;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 using Contracts;
 using Entity;
 using HttpManager.Extensions;
+using LoggerService.Extensions;
 using Microsoft.Extensions.Options;
-using Shared.Dto.User;
 using Shared.RequestFeatures;
 
 namespace HttpManager;
 
 public abstract class HttpClientBase<TEntity> : IHttpClientBase<TEntity>, IAsyncDisposable
 {
+    private readonly JsonSerializerOptions _jsonSerializerOptions;
+    
     protected readonly IOptions<ApiEndpointsOptions> Endpoints;
+    protected readonly ILoggerManager Logger;
     protected readonly HttpClient Client;
-
-    protected HttpClientBase(IHttpClientFactory factory, IOptions<ApiEndpointsOptions> endpointOptions)
+    
+    protected HttpClientBase(IHttpClientFactory factory, IOptions<ApiEndpointsOptions> endpointOptions, ILoggerManager logger)
     {
+        Logger = logger;
         Endpoints = endpointOptions;
         Client = factory.CreateClient();
         Client.ConfigureHttpClient(endpointOptions);
+        
+        _jsonSerializerOptions = new JsonSerializerOptions
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault
+        };
+        
+        Logger.LogInfo($"[{nameof(HttpClientBase<TEntity>)}] Base url: {Client.BaseAddress}");
     }
 
-    public async Task<TEntity?> GetByIdAsync(Uri endpoint) =>
-        await Client.GetFromJsonAsync<TEntity>(endpoint);
-
-    public async Task<PagedList<TEntity>?> GetAllAsync(Uri endpoint)
+    public async Task<TEntity?> GetByIdAsync(Uri endpoint)
     {
-        var result = await Client.GetAsync(endpoint);
-        var list = await result.Content.ReadFromJsonAsync<List<TEntity>>();
-        result.Headers.TryGetValues("X-Pagination", out var values);
-        var metadata = JsonSerializer.Deserialize<PaginationMetaData>(values?.ElementAt(0) ?? "");
+        Logger.LogApiTransactionStart(nameof(GetByIdAsync), endpoint);
+        var response = await Client.GetAsync(endpoint);
         
-        return new PagedList<TEntity>(metadata.TotalCount, metadata.CurrentPage, metadata.PageSize, list);
+        Logger.LogStatusCode(nameof(GetByIdAsync), response.StatusCode);
+        response.EnsureSuccessStatusCode();
+        
+        var result = 
+            await response.Content.ReadFromJsonAsync<TEntity>(_jsonSerializerOptions) 
+            ?? default;
+        Logger.LogApiCallHeaders(nameof(GetByIdAsync), response);
+        Logger.LogApiCallContent<TEntity>(nameof(GetByIdAsync), result);
+        
+        Logger.LogApiTransactionEnd(nameof(GetByIdAsync));
+        
+        return result;
+    }
+
+    public async Task<PagedList<TEntity>> GetAllAsync(Uri endpoint)
+    {
+        Logger.LogApiTransactionStart(nameof(GetAllAsync), endpoint);
+        var response = await Client.GetAsync(endpoint);
+        
+        Logger.LogStatusCode(nameof(GetAllAsync), response.StatusCode);
+        response.EnsureSuccessStatusCode();
+        
+        var result = 
+            await response.Content.ReadFromJsonAsync<List<TEntity>>(_jsonSerializerOptions) 
+            ?? [];
+        Logger.LogApiCallHeaders(nameof(GetAllAsync), response);
+        Logger.LogApiCallContent(nameof(GetAllAsync), result);
+        
+        var metadata = ExtractPaginationMetaData(response.Headers);
+
+        Logger.LogApiTransactionEnd(nameof(GetAllAsync));
+        return new PagedList<TEntity>(metadata.TotalCount, metadata.CurrentPage, metadata.PageSize, result);
     }
 
     public async Task<TEntity?> CreateAsync(Uri endpoint, TEntity entity)
     {
-        var result = await Client.PostAsJsonAsync(endpoint, entity);
+        Logger.LogApiTransactionStart(nameof(CreateAsync), endpoint);
+        Logger.LogPostData(nameof(CreateAsync), entity);
+        var response = await Client.PostAsJsonAsync(endpoint, entity, _jsonSerializerOptions);
+        
+        Logger.LogStatusCode(nameof(CreateAsync), response.StatusCode);
+        response.EnsureSuccessStatusCode();
 
-        return await result.Content.ReadFromJsonAsync<TEntity>();
+        var result = 
+            await response.Content.ReadFromJsonAsync<TEntity>(_jsonSerializerOptions)
+            ?? default;
+        Logger.LogApiCallHeaders(nameof(CreateAsync), response);
+        Logger.LogApiCallContent<TEntity>(nameof(CreateAsync), result);
+
+        Logger.LogApiTransactionEnd(nameof(CreateAsync));
+        return result;
     }
     
-    public async Task UpdateAsync(Uri endpoint, TEntity entity) =>
-        await Client.PutAsJsonAsync(endpoint, entity);
-    
-    public async Task DeleteAsync(Uri uri) =>
-        await Client.DeleteAsync(uri);
+    public async Task UpdateAsync(Uri endpoint, TEntity? entity)
+    {
+        ArgumentNullException.ThrowIfNull(entity);
+        
+        Logger.LogApiTransactionStart(nameof(UpdateAsync), endpoint);
+        Logger.LogPostData(nameof(UpdateAsync), entity);
+        var response = await Client.PutAsJsonAsync(endpoint, entity, _jsonSerializerOptions);
+        
+        Logger.LogStatusCode(nameof(UpdateAsync), response.StatusCode);
+        response.EnsureSuccessStatusCode();
+        
+        Logger.LogApiCallHeaders(nameof(UpdateAsync), response);
+        Logger.LogApiTransactionEnd(nameof(UpdateAsync));
+    }
+
+    public async Task DeleteAsync(Uri endpoint)
+    {
+        Logger.LogApiTransactionStart(nameof(DeleteAsync), endpoint);
+        var response = await Client.DeleteAsync(endpoint);
+        
+        Logger.LogStatusCode(nameof(DeleteAsync), response.StatusCode);
+        response.EnsureSuccessStatusCode();
+        
+        Logger.LogApiCallHeaders(nameof(DeleteAsync), response);
+        Logger.LogApiTransactionEnd(nameof(DeleteAsync));
+    }
+
+    private PaginationMetaData ExtractPaginationMetaData(HttpResponseHeaders headers)
+    {
+        const string paginationHeader = "X-Pagination";
+
+        if (headers.TryGetValues(paginationHeader, out var values))
+        {
+            return JsonSerializer.Deserialize<PaginationMetaData>(
+                values.FirstOrDefault() ?? string.Empty, _jsonSerializerOptions)
+                ?? new PaginationMetaData();
+        }
+
+        return new PaginationMetaData();
+    }
 
     #region Dispose
 
@@ -68,52 +155,4 @@ public abstract class HttpClientBase<TEntity> : IHttpClientBase<TEntity>, IAsync
     }
 
     #endregion
-}
-
-public class UserClient : HttpClientBase<UserDto>
-{
-    public UserClient(IHttpClientFactory factory, IOptions<ApiEndpointsOptions> endpointOptions) : base(factory, endpointOptions)
-    {
-    }
-
-    public async Task<UserDto> GetUserByIdAsync(Guid userId, UserParameters? requestParameters = null)
-    {
-        var uri = new Uri($"{Endpoints.Value.Users}/{userId}", UriKind.Relative);
-
-        var result = await GetByIdAsync(uri);
-        
-        return result;
-    }
-
-    public async Task<List<UserDto>> GetAllUsersAsync(UserParameters? requestParameters = null)
-    {
-        var uri = new Uri(Endpoints.Value.Users, UriKind.Relative);
-
-        var result = await GetAllAsync(uri);
-
-        return result.Select(u => u).ToList();
-    }
-
-    public async Task<UserDto> CreateUser(UserDto user)
-    {
-        var uri = new Uri(Endpoints.Value.Users, UriKind.Relative);
-
-        var result = await CreateAsync(uri, user);
-
-        return result;
-    }
-
-    public async Task DeleteUser(Guid userId)
-    {
-        var uri = new Uri($"{Endpoints.Value.Users}/{userId}", UriKind.Relative);
-
-        await DeleteAsync(uri);
-    }
-
-    public async Task UpdateAsync(UserDto user)
-    {
-        var uri = new Uri($"{Endpoints.Value.Users}/{user.Id}", UriKind.Relative);
-
-        await UpdateAsync(uri, user);
-    }
 }
