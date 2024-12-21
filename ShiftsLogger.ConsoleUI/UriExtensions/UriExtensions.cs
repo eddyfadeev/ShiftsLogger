c140704c2634b4220f7a50b6c1f8408c1b026c12
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-21 01:53:08 -0700

    feat: Add UriExtensions.cs and UriExtensions.csproj files
    Added UriExtensions.cs and UriExtensions.csproj files to handle Uri related operations, including appending GUID and adding query parameters.

---
+++ ShiftsLogger.ConsoleUI/UriExtensions/UriExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Shared.RequestFeatures;
+using UriExtensions.Utility;
+
+namespace UriExtensions;
+
+public static class UriExtensions
+{
+    public static Uri AppendGuid(this Uri uri, Guid guid) =>
+        new ($"{uri.OriginalString.TrimEnd('/')}/{guid}", UriKind.Relative);
+
+    public static Uri AddQueryParameters<T>(this Uri uri, T? entity) where T : RequestParameters =>
+        new($"{uri.OriginalString}?{QueryBuilder.BuildParametersQuery(entity)}", UriKind.Relative);
+}
