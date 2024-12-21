0070e817e1b7ccabc5a7f780b08a0155fe8ca805
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-21 01:54:33 -0700

    feat(uri): add QueryBuilder utility class for building query parameters based on entity properties.

---
+++ ShiftsLogger.ConsoleUI/UriExtensions/Utility/QueryBuilder.cs
@@ -0,0 +1,41 @@
+﻿using System.Globalization;
+using System.Reflection;
+using Shared.RequestFeatures;
+
+namespace UriExtensions.Utility;
+
+public static class QueryBuilder
+{
+    public static string BuildParametersQuery<T>(T? entity)
+        where T : RequestParameters
+    {
+        if (entity is null)
+        {
+            return string.Empty;
+        }
+        
+        var properties = entity.GetType()
+            .GetProperties(BindingFlags.Instance | BindingFlags.Public).Where(prop => 
+                prop.PropertyType != typeof(bool) &&
+                prop.GetValue(entity) is not null &&
+                prop.GetValue(entity) != default &&
+                !(prop.GetValue(entity) is string strValue && string.IsNullOrWhiteSpace(strValue)))
+            .ToList();
+
+        List<string> queryParameters = [];
+
+        foreach (var property in properties)
+        {
+            var rawValue = property.GetValue(entity);
+            
+            string value = 
+                rawValue is DateTime dateTime 
+                    ? dateTime.ToString("yyyy-MM-dd", CultureInfo.InvariantCulture)
+                    : rawValue?.ToString() ?? $"{property.Name}: Error getting value!";
+            
+            queryParameters.Add($"{Uri.EscapeDataString(property.Name.ToLower())}={Uri.EscapeDataString(value)}");
+        }
+
+        return string.Join('&', queryParameters);
+    }
+}
