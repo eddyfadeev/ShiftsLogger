8d55a5934e79f1e43ccea6bcedba71d9687721ca
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-23 00:20:39 -0700

    feat(view): add TableSettings class with properties for customizing table appearance and separator row validation logic.

---
+++ ShiftsLogger.ConsoleUI/View/View.Entity/TableSettings.cs
@@ -0,0 +1,28 @@
+﻿using Spectre.Console;
+
+namespace View.Entity;
+
+public class TableSettings
+{
+    private int _separatorRow = 5;
+    
+    public bool ShowFooter { get; set; } = false;
+    public TableBorder Border { get; set; } = TableBorder.Rounded;
+    public bool Expand { get; set; } = false;
+    public bool ShowTitle { get; set; } = false;
+
+    public int SeparatorRow
+    {
+        get => _separatorRow;
+        set
+        {
+            const int minSeparatorRow  = 1;
+            const int maxSeparatorRow  = 10;
+            const int defaultSeparatorRow = 5;
+            
+            _separatorRow = value is 
+                > minSeparatorRow and < maxSeparatorRow 
+                ? value : defaultSeparatorRow;
+        }
+    }
+}
