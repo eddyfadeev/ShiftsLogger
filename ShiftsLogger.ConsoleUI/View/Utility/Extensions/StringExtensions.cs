698f8590e46697a5988f1097c6abe4e4fd730bcb
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-23 00:21:37 -0700

    feat: Add StringExtensions class with methods for splitting camel case strings, colorizing and decolorizing selected options.

---
+++ ShiftsLogger.ConsoleUI/View/Utility/Extensions/StringExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+
+namespace Utility.Extensions;
+
+public static class StringExtensions
+{
+    public static string[] SplitCamelCase(this string[] cameCaseStrings) =>
+        cameCaseStrings.Select(str => str.SplitCamelCase()).ToArray();
+    
+    public static string SplitCamelCase(this string camelCaseString)
+    {
+        var builder = new StringBuilder();
+
+        foreach (char character in camelCaseString)
+        {
+            if (char.IsUpper(character))
+            {
+                builder.Append(' ');
+            }
+
+            builder.Append(character);
+        }
+
+        return builder.ToString().Trim();
+    }
+    
+    public static string[] ColorizeSelectedOption(this string[] strings) =>
+        strings.Select(str => str.ColorizeSelectedOption()).ToArray();
+
+    public static string ColorizeSelectedOption(this string str) =>
+        $"[gold3_1]{str}[/]";
+    
+    public static string[] DecolorizeSelectedOption(this string[] strings) =>
+        strings.Select(str => str.DecolorizeSelectedOption()).ToArray();
+    
+    public static string DecolorizeSelectedOption(this string str) =>
+        str.Replace("[gold3_1]", "").Replace("[/]", "");
+}
