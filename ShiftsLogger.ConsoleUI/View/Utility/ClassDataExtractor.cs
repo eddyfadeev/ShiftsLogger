1545ee05e6d6f65a607ad69b293f90508dcc16f4
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-23 00:21:15 -0700

    refactor: move ClassDataExtractor.cs to a different folder and update namespace to simplify project structure and improve organization. Fix method to retrieve property values as strings. Add caching mechanism for property information.

---
+++ ShiftsLogger.ConsoleUI/View/Utility/ClassDataExtractor.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+
+namespace Utility;
+
+public static class ClassDataExtractor
+{
+    private static readonly Dictionary<Type, PropertyInfo[]> PropertyCache = new();
+
+    public static IEnumerable<string> GetPropertyNames<T>(T obj) =>
+        obj is null 
+            ? Array.Empty<string>() 
+            : GetTypeProperties(obj).Select(prop => prop.Name);
+
+    public static IEnumerable<string> GetPropertyValuesAsString<T>(T obj, params string[] columnsToIgnore) =>
+        obj is null 
+            ? Array.Empty<string>() 
+            : GetTypeProperties(obj)
+                .Where(prop => !columnsToIgnore.Contains(prop.Name))
+                .Select(prop => prop.GetValue(obj)?.ToString() ?? string.Empty);
+    
+    private static PropertyInfo[] GetTypeProperties<T>(T obj)
+    {
+        var type = obj!.GetType();
+        if (!PropertyCache.TryGetValue(type, out var properties))
+        {
+            properties = type.GetProperties(BindingFlags.Instance | BindingFlags.Public);
+            PropertyCache[type] = properties;
+        }
+        
+        return properties;
+    }
+}
