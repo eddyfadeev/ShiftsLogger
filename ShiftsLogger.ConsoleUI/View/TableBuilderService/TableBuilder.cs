f4dbec0ab404d950523253462f691e904b410952
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-23 00:22:05 -0700

    Refactor TableBuilder.cs to use TableData<T> object for building a table with configurable settings and data rows.

--- ShiftsLogger.ConsoleUI/View/TableBuilderService/TableBuilder.cs
+++ ShiftsLogger.ConsoleUI/View/TableBuilderService/TableBuilder.cs
@@ -1,50 +1,39 @@
 ﻿using Spectre.Console;
-using TableBuilderService.Extensions;
-using TableBuilderService.Utility;
 using View.Contracts;
+using View.Entity;
 
 namespace TableBuilderService;
 
 public class TableBuilder : ITableBuilder
 {
-    public Table Build<T>(params List<T> tableData)
+    public Table Build<T>(TableData<T> tableData)
     {
-        if (tableData.Count < 1)
+        if (tableData.ObjectsList.Count < 1)
         {
             throw new ArgumentException("No data provided to build a table", nameof(tableData));
         }
         
-        var table = new Table();
-        table.ApplyDefaultConfiguration();
-        
-        var columnNames = ClassDataExtractor.GetPropertyNames(tableData[0]).ToArray();
-        table.AddColumns(columnNames);
-
-        for (int i = 0; i < tableData.Count; i++)
+        var table = new Table
         {
-            if (i is not 0 && i % 5 is 0)
+            ShowHeaders = tableData.Settings.ShowTitle,
+            ShowFooters = tableData.Settings.ShowFooter,
+            Expand = tableData.Settings.Expand,
+            Title = tableData.Title,
+            Caption = tableData.Footer
+        };
+        
+        table.Border(tableData.Settings.Border);
+        table.AddColumns(tableData.TableColumnHeaders);
+        
+        for (int i = 0; i < tableData.TableDataRows.Length; i++)
+        {
+            if (i is not 0 && i % tableData.Settings.SeparatorRow is 0)
             {
                 table.AddEmptyRow();
             }
             
-            var data = ClassDataExtractor.GetPropertyValuesAsString(tableData[i]).ToArray();
-            
-            table.AddRow(data);
+            table.AddRow(tableData.TableDataRows[i]);
         }
-
-        var sortKeys = columnNames.Select((name, index) => $"({index + 1}) - {name}");
-        
-        // TODO: Extract this into a separate class or method
-        var footer = $"""
-                      [white]
-                      Press ESC to return to previous menu
-                      Press right arrow to view next page or left arrow to return to the previous page
-                      Press (key) to toggle sorting
-                      {string.Join(" | ", sortKeys)}
-                      [/]
-                      """;
-        
-        table.Caption(new TableTitle(footer));
 
         return table;
     }
