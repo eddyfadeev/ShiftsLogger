ab616c1bcbc54dc41a5df02b33fb7c872bb7964d
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-23 23:16:53 -0700

    Refactor TableData class to make it abstract, update TableColumnHeaders and TableDataRows to be abstract properties, and make SelectedIndex setter more robust.

--- ShiftsLogger.ConsoleUI/View/View.ViewModel/TableData.cs
+++ ShiftsLogger.ConsoleUI/View/View.ViewModel/TableData.cs
@@ -1,40 +1,51 @@
-﻿using Shared.RequestFeatures;
-using Spectre.Console;
+﻿using Spectre.Console;
 using View.Entity;
-using View.Utility;
 using View.Utility.Extensions;
 
 namespace View.ViewModel;
 
-public class TableData<T>
+public abstract class TableData<T>
 {
     private int _selectedIndex;
     
-    public PagedList<T> ObjectsList { get; }
+    public abstract StyledString[] TableColumnHeaders { get; protected set; }
+    public abstract StyledString[][] TableDataRows { get; protected set; }
+    
+    public List<T> ObjectsList { get; }
     public TableSettings Settings { get; set; }
-    public StyledString[] TableColumnHeaders { get; private set; }
-    public StyledString[][] TableDataRows { get; private set; }
 
-    public int SelectedIndex
+    public int SelectedIndex 
     {
         get => _selectedIndex;
         set
         {
             const int minIndex = 0;
             int maxIndex = ObjectsList.Count - 1;
-            int previousSelection = _selectedIndex;
+            int previousIndex = _selectedIndex;
 
-            _selectedIndex = Math.Clamp(value, minIndex, maxIndex);
+            int newIndex = Math.Clamp(value, minIndex, maxIndex);
 
-            TableDataRows[previousSelection] = TableDataRows[previousSelection].RemoveStyle(Settings.ContentStyle);
-            TableDataRows[_selectedIndex] = TableDataRows[_selectedIndex].ApplyStyle(Settings.SelectionStyle);
+            if (newIndex == previousIndex)
+            {
+                return;
+            }
+
+            _selectedIndex = newIndex; 
+
+            TableDataRows[previousIndex] = 
+                TableDataRows[previousIndex]
+                    .RemoveStyle(Settings.ContentStyle);
+            
+            TableDataRows[_selectedIndex] = 
+                TableDataRows[_selectedIndex]
+                    .ApplyStyle(Settings.SelectionStyle);
         }
     }
-
+    
     public TableTitle Title { get; set; } = new (string.Empty);
     public TableTitle Footer { get; set; } = new (string.Empty);
     
-    public TableData(PagedList<T> objectsList, TableSettings tableSettings) 
+    protected TableData(List<T> objectsList, TableSettings tableSettings) 
     {
         if (objectsList.Count <= 0)
         {
@@ -43,44 +54,5 @@ public class TableData<T>
         
         Settings = tableSettings;
         ObjectsList = objectsList;
-        TableColumnHeaders = GetColumnHeaders(objectsList[0]);
-        TableDataRows = GetTableData(objectsList);
     }
-
-    public bool RemoveColumn(string columnName)
-    {
-        StyledString? columnToRemove = Array.Find(
-            TableColumnHeaders, s => 
-            s.OriginString.Equals(columnName, StringComparison.InvariantCultureIgnoreCase));
-        
-        if (columnToRemove is null)
-        {
-            return false;
-        }
-
-        TableColumnHeaders = GetColumnHeaders(ObjectsList[0], columnToRemove);
-        TableDataRows = GetTableData(ObjectsList, columnToRemove);
-
-        return true;
-    }
-
-    private static StyledString[] GetColumnHeaders(T obj, params string[] columnsToIgnore) =>
-        ClassDataExtractor
-            .GetPropertyNames(obj)
-            .Where(s => !columnsToIgnore.Contains(s))
-            .Select(str => new StyledString(str.SplitCamelCase()))
-            .ToArray();
-
-    private StyledString[][] GetTableData(PagedList<T> pagedList, params string[] columnsToIgnore) =>
-        pagedList.Select((item, index) =>
-        {
-            var dataRow = 
-                ClassDataExtractor
-                    .GetPropertyValuesAsString(item, columnsToIgnore)
-                    .ToArray();
-
-            return index == SelectedIndex
-                ? dataRow.ApplyStyle(Settings.SelectionStyle)
-                : dataRow.ApplyStyle(Settings.ContentStyle);
-        }).ToArray();
 }
