1693e952de8ff94c2fa8bf790298b880e8fb7949
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-23 21:57:30 -0700

    feat: Add StyledStringExtensions class with methods to apply and remove styles from StyledString objects.

---
+++ ShiftsLogger.ConsoleUI/View/View.Utility/Extensions/StyledStringExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Spectre.Console;
+using View.Entity;
+
+namespace View.Utility.Extensions;
+
+public static class StyledStringExtensions
+{
+    public static StyledString[] ApplyStyle(this StyledString[] strings, Style? textStyle) =>
+        strings
+            .Select(str => str.ApplyStyle(textStyle))
+            .ToArray();
+
+    public static StyledString ApplyStyle(this StyledString str, Style? textStyle) =>
+        new (str, textStyle);
+
+    public static StyledString[] RemoveStyle(this StyledString[] strings, Style? style = null) =>
+        strings
+            .Select(str => str.RemoveStyle(style))
+            .ToArray();
+
+    public static StyledString RemoveStyle(this StyledString text, Style? style = null) =>
+        new(text.OriginString, style);
+}
