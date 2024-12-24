981c49ae5ce18758e1d9799a2ae9653aaaaf4aae
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-23 23:17:06 -0700

    feat(menu): add MenuTable class to handle menu entries in the console UI view model. The class includes methods to generate table data rows based on menu entries and apply styles based on selection.

---
+++ ShiftsLogger.ConsoleUI/View/View.ViewModel/MenuTable.cs
@@ -0,0 +1,30 @@
+﻿using View.Entity;
+using View.Utility.Extensions;
+
+namespace View.ViewModel;
+
+public sealed class MenuTable : TableData<string>
+{
+    public override StyledString[] TableColumnHeaders { get; protected set; }
+    public override StyledString[][] TableDataRows { get; protected set; }
+    
+    public MenuTable(List<string> menuEntries, TableSettings tableSettings) 
+        : base(menuEntries, tableSettings)
+    {
+        TableColumnHeaders = [string.Empty];
+        TableDataRows = GetMenuEntries(menuEntries);
+    }
+    
+    private StyledString[][] GetMenuEntries(List<string> menuEntries)
+    {
+        List<string[]> preparedEntries = [];
+        preparedEntries.AddRange(menuEntries.Select(entry => (string[]) [entry]));
+        
+        return preparedEntries.Select(
+                (entry, index) =>
+                    index == SelectedIndex
+                        ? entry.ApplyStyle(Settings.SelectionStyle)
+                        : entry.ApplyStyle(Settings.ContentStyle))
+            .ToArray();
+    }
+}
