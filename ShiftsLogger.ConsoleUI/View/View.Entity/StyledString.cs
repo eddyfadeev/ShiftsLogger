da6f549efa5884aa9c9a35d24440f5415889b300
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-23 21:57:24 -0700

    feat(view): add StyledString entity for representing styled text with Spectre.Console library.

---
+++ ShiftsLogger.ConsoleUI/View/View.Entity/StyledString.cs
@@ -0,0 +1,35 @@
+﻿using Spectre.Console;
+
+namespace View.Entity;
+
+public readonly record struct StyledString
+{
+    private readonly string _originString;
+
+    public string OriginString
+    {
+        get => _originString;
+        init => _originString = string.IsNullOrWhiteSpace(value) 
+            ? string.Empty 
+            : value;
+    }
+
+    public Style? AppliedStyle { get; init; }
+    
+    public Markup StyledText => 
+        new(OriginString, AppliedStyle);
+    
+    public StyledString(string stringToStyle, Style? style = null)
+    {
+        OriginString = stringToStyle;
+        AppliedStyle = style;
+    }
+    
+    public StyledString() {}
+    
+    public static implicit operator StyledString(string originString) => 
+        new(originString);
+    
+    public static implicit operator string(StyledString styledString) =>
+        styledString.OriginString;
+}
