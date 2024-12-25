926bb426da33aa64010b475af07f41bd2d49236c
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-24 20:59:59 -0700

    feat(view): add MenuSettings class for customizing table settings in the console UI.

---
+++ ShiftsLogger.ConsoleUI/View/View.Menu/MenuSettings.cs
@@ -0,0 +1,22 @@
+﻿using Spectre.Console;
+using View.Entity;
+
+namespace View.Menu;
+
+public sealed class MenuSettings : TableSettings
+{
+    public MenuSettings()
+    {
+        ShowColumnHeaders = false;
+        ShowFooter = true;
+        Expand = true;
+        BorderStyle = TableBorder.Rounded;
+        TitleStyle = new Style(foreground: Color.Olive);
+        TitleAlignment = HorizontalAlignment.Center;
+        HeadersStyle = null;
+        ContentStyle = new Style(foreground: Color.White);
+        ContentAlignment = HorizontalAlignment.Center;
+        SelectionStyle = new Style(foreground: Color.Gold3_1);
+        SeparatorRow = 0;
+    }
+}
