dfe9c24e2586b109fd8953c17a044049564c7a6c
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-24 21:01:14 -0700

    feat: Add support for horizontal alignment in ApplyStyle and RemoveStyle methods in StyledStringExtensions.cs.

--- ShiftsLogger.ConsoleUI/View/View.Utility/Extensions/StyledStringExtensions.cs
+++ ShiftsLogger.ConsoleUI/View/View.Utility/Extensions/StyledStringExtensions.cs
@@ -5,19 +5,19 @@ namespace View.Utility.Extensions;
 
 public static class StyledStringExtensions
 {
-    public static StyledString[] ApplyStyle(this StyledString[] strings, Style? textStyle) =>
+    public static StyledString[] ApplyStyle(this StyledString[] strings, Style? textStyle, HorizontalAlignment alignment = HorizontalAlignment.Left) =>
         strings
-            .Select(str => str.ApplyStyle(textStyle))
+            .Select(str => str.ApplyStyle(textStyle, alignment))
             .ToArray();
 
-    public static StyledString ApplyStyle(this StyledString str, Style? textStyle) =>
-        new (str, textStyle);
+    public static StyledString ApplyStyle(this StyledString str, Style? textStyle, HorizontalAlignment alignment) =>
+        new (str, textStyle, alignment);
 
-    public static StyledString[] RemoveStyle(this StyledString[] strings, Style? style = null) =>
+    public static StyledString[] RemoveStyle(this StyledString[] strings, Style? style = null, HorizontalAlignment alignment = HorizontalAlignment.Left) =>
         strings
-            .Select(str => str.RemoveStyle(style))
+            .Select(str => str.RemoveStyle(style, alignment))
             .ToArray();
 
-    public static StyledString RemoveStyle(this StyledString text, Style? style = null) =>
-        new(text.OriginString, style);
+    public static StyledString RemoveStyle(this StyledString text, Style? style = null, HorizontalAlignment alignment = HorizontalAlignment.Left) =>
+        new(text.OriginString, style, alignment);
 }
