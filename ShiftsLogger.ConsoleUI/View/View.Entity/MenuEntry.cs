5632be586a7fa92c73fbc29ab805fba6c77c472d
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-24 20:59:50 -0700

    feat(view): add MenuEntry class to View.Entity folder.

---
+++ ShiftsLogger.ConsoleUI/View/View.Entity/MenuEntry.cs
@@ -0,0 +1,13 @@
+﻿namespace View.Entity;
+
+public class MenuEntry
+{
+    public string Name { get; set; }
+    public Action? Action { get; set; }
+
+    public MenuEntry(string name, Action? action = null)
+    {
+        Name = name;
+        Action = action;
+    }
+}
