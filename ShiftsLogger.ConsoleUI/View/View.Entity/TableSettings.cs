832b36e25d23529f6f4deb78ed577492ddfe317b
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-24 21:00:54 -0700

    Refactor TableSettings class by changing it to an abstract class and adding alignment properties for Title, Headers, Content, and Selection styles.

--- ShiftsLogger.ConsoleUI/View/View.Entity/TableSettings.cs
+++ ShiftsLogger.ConsoleUI/View/View.Entity/TableSettings.cs
@@ -2,7 +2,7 @@
 
 namespace View.Entity;
 
-public class TableSettings
+public abstract class TableSettings
 {
     private int _separatorRow = 5;
     
@@ -12,10 +12,17 @@ public class TableSettings
     public bool RowSeparatorIsActive => 
         _separatorRow > 0 ;
     public TableBorder BorderStyle { get; set; } = TableBorder.Rounded;
+    
+    // TODO: Extract to style struct 
     public Style? TitleStyle { get; set; } = null;
+    public HorizontalAlignment TitleAlignment { get; set; } = HorizontalAlignment.Left;
     public Style? HeadersStyle { get; set; } = null;
+    public HorizontalAlignment HeadersAlignment { get; set; } = HorizontalAlignment.Left;
     public Style? ContentStyle { get; set; } = null;
+    public HorizontalAlignment ContentAlignment { get; set; } = HorizontalAlignment.Left;
     public Style? SelectionStyle { get; set; } = null;
+    public HorizontalAlignment SelectionAlignment { get; set; } = HorizontalAlignment.Left;
+    // End of extraction
 
     public int SeparatorRow
     {
