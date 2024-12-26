ac2d698fd1e277d038ba8dbae0a1e245dcd8ebc6
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-25 21:23:26 -0700

    refactor(MenuTable): Refactor MenuTable class to use StyledString instead of string for menu entries and table headers, update GetMenuEntries method accordingly.

--- ShiftsLogger.ConsoleUI/View/View.ViewModel/MenuTable.cs
+++ ShiftsLogger.ConsoleUI/View/View.ViewModel/MenuTable.cs
@@ -1,30 +1,31 @@
 ﻿using View.Entity;
+using View.Entity.Structures;
 using View.Utility.Extensions;
 
 namespace View.ViewModel;
 
-public sealed class MenuTable : TableData<string>
+public sealed class MenuTable : TableData<StyledString>
 {
     public override StyledString[] TableColumnHeaders { get; protected set; }
     public override StyledString[][] TableDataRows { get; protected set; }
     
-    public MenuTable(List<string> menuEntries, TableSettings tableSettings) 
+    public MenuTable(List<StyledString> menuEntries, TableSettings tableSettings) 
         : base(menuEntries, tableSettings)
     {
-        TableColumnHeaders = [string.Empty];
+        TableColumnHeaders = [new StyledString()];
         TableDataRows = GetMenuEntries(menuEntries);
     }
     
-    private StyledString[][] GetMenuEntries(List<string> menuEntries)
+    private StyledString[][] GetMenuEntries(List<StyledString> menuEntries)
     {
-        List<string[]> preparedEntries = [];
-        preparedEntries.AddRange(menuEntries.Select(entry => (string[]) [entry]));
+        List<StyledString[]> preparedEntries = [];
+        preparedEntries.AddRange(menuEntries.Select(entry => (StyledString[]) [entry]));
         
         return preparedEntries.Select(
                 (entry, index) =>
                     index == SelectedIndex
-                        ? entry.ApplyStyle(Settings.SelectionStyle, Settings.ContentAlignment)
-                        : entry.ApplyStyle(Settings.ContentStyle, Settings.ContentAlignment))
+                        ? entry.ApplyStyle(Settings.SelectionStyle)
+                        : entry.ApplyStyle(Settings.ContentStyle))
             .ToArray();
     }
 }
