2d6fedb4f105c35f88d1d26c16f3ef8b1d334bc7
Eduard Fadieiev <eddy.fadeev@gmail.com>
2024-12-26 21:54:05 -0700

    feat(presentation): Add StylingData and StaticData classes
    Added StylingData class with default styles for title, menu entry, header, and selection.
    Added StaticData class with constants for in-menu footer and entries display footer.

---
+++ ShiftsLogger.ConsoleUI/ShiftsLogger.Presentation/StylingData.cs
@@ -0,0 +1,31 @@
+﻿using Spectre.Console;
+using View.Entity.Structures;
+
+namespace ShiftsLogger.Presentation;
+
+internal static class StylingData
+{
+    internal static readonly StringStyling DefaultTitleStyling = new()
+    {
+        AppliedStyle = new Style(foreground: Color.Wheat1),
+        HorizontalAlignment = HorizontalAlignment.Center
+    };
+    
+    internal static readonly StringStyling DefaultMenuEntryStyling = new()
+    {
+        AppliedStyle = new Style(foreground: Color.White),
+        HorizontalAlignment = HorizontalAlignment.Center
+    };
+
+    internal static readonly StringStyling DefaultHeaderStyling = new()
+    {
+        AppliedStyle = new Style(foreground: Color.Wheat1),
+        HorizontalAlignment = HorizontalAlignment.Center
+    };
+
+    internal static readonly StringStyling DefaultSelectionStyling = new()
+    {
+        AppliedStyle = new Style(foreground: Color.Gold3_1),
+        HorizontalAlignment = HorizontalAlignment.Center
+    };
+}
