f86138de47ba57b37bbd4a13a81a4ba666bf454e
Eduard Fadieiev <eddy.fadeev@gmail.com>
2025-01-01 21:06:20 -0700

    feat(view): add MenuSettings class for customizing menu appearance.

---
+++ ShiftsLogger.ConsoleUI/View/View.Menu/MenuSettings.cs
@@ -0,0 +1,19 @@
+﻿using Spectre.Console;
+using View.Entity;
+
+namespace View.Menu;
+
+public sealed class MenuSettings : RenderSettings
+{
+    public MenuSettings()
+    {
+        ShowFooter = true;
+        Expand = true;
+        BorderStyle = TableBorder.Rounded;
+        TitleStyle = new Style(foreground: Color.Olive);
+        FooterStyle = new Style(foreground: Color.White);
+        ContentStyle = new Style(foreground: Color.White);
+        SelectionStyle = new Style(foreground: Color.Gold3_1);
+        SeparatorRow = 0;
+    }
+}
