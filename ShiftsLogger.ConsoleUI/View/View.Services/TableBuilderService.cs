944ed29a604723bffdaf28179f52f56dc3338571
Eduard Fadieiev <eddy.fadeev@gmail.com>
2025-01-04 21:06:25 -0700

    feat(view): add TableBuilderService class for building tables in the console UI.

---
+++ ShiftsLogger.ConsoleUI/View/View.Services/TableBuilderService.cs
@@ -0,0 +1,11 @@
+﻿using Spectre.Console;
+using View.Entity;
+using View.Services.Contracts;
+
+namespace View.Services;
+
+public class TableBuilderService : ITableBuilder
+{
+    public Table Build<T>(TableData<T> tableData, ITableBuilderStrategy<T> strategy) =>
+        strategy.Build(tableData);
+}
