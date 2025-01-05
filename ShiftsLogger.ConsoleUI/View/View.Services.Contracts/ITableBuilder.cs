a63e81ad88d075462cc1ecb6847d09ff8fddf280
Eduard Fadieiev <eddy.fadeev@gmail.com>
2025-01-04 21:02:08 -0700

    feat(console): add ITableBuilder interface for building tables in console UI.

---
+++ ShiftsLogger.ConsoleUI/View/View.Services.Contracts/ITableBuilder.cs
@@ -0,0 +1,9 @@
+﻿using Spectre.Console;
+using View.Entity;
+
+namespace View.Services.Contracts;
+
+public interface ITableBuilder
+{
+    Table Build<T>(TableData<T> tableData, ITableBuilderStrategy<T> strategy);
+}
