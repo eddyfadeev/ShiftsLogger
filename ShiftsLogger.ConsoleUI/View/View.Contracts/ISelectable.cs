3f31493e4d8722474a5a187faf2372c9e8115ed2
Eduard Fadieiev <eddy.fadeev@gmail.com>
2025-01-04 21:01:44 -0700

    refactor: rename INavigable interface to ISelectable in View.Contracts directory. Added InvokeAction method.

--- ShiftsLogger.ConsoleUI/View/View.Contracts/ISelectable.cs
+++ ShiftsLogger.ConsoleUI/View/View.Contracts/ISelectable.cs
@@ -1,9 +1,10 @@
 ﻿namespace View.Contracts;
 
-public interface INavigable
+public interface ISelectable
 {
     int SelectedIndex { get; }
     void SelectNext();
     void SelectPrevious();
     void ResetSelection();
+    void InvokeAction();
 }
