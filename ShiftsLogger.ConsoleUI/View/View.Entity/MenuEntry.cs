3bc619bf7c3bff904528f5dfd99e2605cccb43dc
Eduard Fadieiev <eddy.fadeev@gmail.com>
2025-01-04 21:06:02 -0700

    feat(view): implement IActionable interface in MenuEntry struct.

--- ShiftsLogger.ConsoleUI/View/View.Entity/MenuEntry.cs
+++ ShiftsLogger.ConsoleUI/View/View.Entity/MenuEntry.cs
@@ -1,8 +1,9 @@
 ﻿using Spectre.Console;
+using View.Contracts;
 
 namespace View.Entity;
 
-public readonly record struct MenuEntry
+public readonly record struct MenuEntry : IActionable
 {
     public MenuEntry(string entryText, Action? entryAction = null, Style? entryStyle = null)
     {
