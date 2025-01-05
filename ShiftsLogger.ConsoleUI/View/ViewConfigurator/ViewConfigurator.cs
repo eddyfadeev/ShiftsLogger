6b5b5a4feff6b68b8bf42328c8dd49c36360596c
Eduard Fadieiev <eddy.fadeev@gmail.com>
2025-01-04 21:06:31 -0700

    refactor(ViewConfigurator): update service class references in AddView method

--- ShiftsLogger.ConsoleUI/View/ViewConfigurator/ViewConfigurator.cs
+++ ShiftsLogger.ConsoleUI/View/ViewConfigurator/ViewConfigurator.cs
@@ -1,15 +1,11 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
-using View.Contracts.Services;
-using View.TableBuilderService;
-using ViewConfigurator.Factory;
+using View.Services;
+using View.Services.Contracts;
 
 namespace ViewConfigurator;
 
 public static class ViewConfigurator
 {
-    public static void AddView(this IServiceCollection services)
-    {
-        services.AddTransient<ITableBuilder, TableBuilder>();
-        services.AddTransient<IMenuFactory, MenuFactory>();
-    }
+    public static void AddView(this IServiceCollection services) =>
+        services.AddTransient<ITableBuilder, TableBuilderService>();
 }
