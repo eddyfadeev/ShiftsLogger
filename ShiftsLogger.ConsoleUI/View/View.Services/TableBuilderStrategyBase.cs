1610a54e90791dd22b09a3697d4671c6d20fcb55
Eduard Fadieiev <eddy.fadeev@gmail.com>
2025-01-04 21:06:16 -0700

    feat(view): add TableBuilderStrategyBase class for building tables in console UI view services.

---
+++ ShiftsLogger.ConsoleUI/View/View.Services/TableBuilderStrategyBase.cs
@@ -0,0 +1,34 @@
+﻿using Spectre.Console;
+using View.Entity;
+using View.Services.Contracts;
+
+namespace View.Services;
+
+public abstract class TableBuilderStrategyBase<T> : ITableBuilderStrategy<T>
+{
+    public abstract Table Build(TableData<T> tableData);
+    
+    protected Table InitializeTable(TableData<T> tableData) =>
+        new ()
+        {
+            Border = tableData.Settings.BorderStyle,
+            ShowHeaders = tableData.Settings.ShowHeaders,
+            ShowFooters = tableData.Settings.ShowFooter,
+            Expand = tableData.Settings.Expand,
+            Title = tableData.Title,
+            Caption = tableData.Footer
+        };
+
+    protected void ValidateTableData(TableData<T> tableData)
+    {
+        ArgumentNullException.ThrowIfNull(tableData, nameof(tableData));
+        ArgumentNullException.ThrowIfNull(tableData.Settings, nameof(tableData.Settings));
+
+        if (tableData.Count < 1)
+        {
+            throw new ArgumentException("Table data must contain at least one item.", nameof(tableData));
+        }
+        
+        ArgumentNullException.ThrowIfNull(tableData.FirstOrDefault());
+    }
+}
