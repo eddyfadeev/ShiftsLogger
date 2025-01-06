d5ef03103f2fafd3822ad961a22ae768235c94e3
Eduard Fadieiev <eddy.fadeev@gmail.com>
2025-01-05 20:34:11 -0700

    feat: Add UsersMenu class to handle user menu display and interaction in the console UI.

---
+++ ShiftsLogger.ConsoleUI/ShiftsLogger.Presentation/UsersMenu/UsersMenu.cs
@@ -0,0 +1,25 @@
+﻿using View.Entity;
+using View.Menu;
+
+namespace ShiftsLogger.Presentation.UsersMenu;
+
+public sealed class UsersMenu : MenuViewBase<Menu, MenuEntry>
+{
+    public UsersMenu(IServiceProvider serviceProvider) 
+        : base(serviceProvider)
+    {
+    }
+    
+    protected override Menu CreateMenu()
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override IReadOnlyCollection<MenuEntry> GetMenuEntries()
+    {
+        throw new NotImplementedException();
+    }
+    
+    protected override TableSettings CreateSettings() =>
+        new MenuSettings();
+}
