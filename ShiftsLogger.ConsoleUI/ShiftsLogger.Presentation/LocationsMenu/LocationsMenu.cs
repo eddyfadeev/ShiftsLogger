8ecbbd9d17423d444b82a32dd495dccfcc63d3be
Eduard Fadieiev <eddy.fadeev@gmail.com>
2025-01-05 20:34:21 -0700

    feat(presentation): add LocationsMenu class for managing menu entries in the LocationsMenu namespace.

---
+++ ShiftsLogger.ConsoleUI/ShiftsLogger.Presentation/LocationsMenu/LocationsMenu.cs
@@ -0,0 +1,24 @@
+﻿using View.Entity;
+using View.Menu;
+
+namespace ShiftsLogger.Presentation.LocationsMenu;
+
+public sealed class LocationsMenu : MenuViewBase<Menu, MenuEntry>
+{
+    public LocationsMenu(IServiceProvider serviceProvider) 
+        : base(serviceProvider)
+    {
+    }
+    protected override Menu CreateMenu()
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override IReadOnlyCollection<MenuEntry> GetMenuEntries()
+    {
+        throw new NotImplementedException();
+    }
+    
+    protected override TableSettings CreateSettings() =>
+        new MenuSettings();
+}
