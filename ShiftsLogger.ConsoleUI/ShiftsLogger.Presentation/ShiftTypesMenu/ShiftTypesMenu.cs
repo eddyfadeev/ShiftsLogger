bbe95881bfd5ad155560bf04b7dcdaf8113343df
Eduard Fadieiev <eddy.fadeev@gmail.com>
2025-01-05 20:34:16 -0700

    feat(presentation): add ShiftTypesMenu class for managing shift types in the console UI.

---
+++ ShiftsLogger.ConsoleUI/ShiftsLogger.Presentation/ShiftTypesMenu/ShiftTypesMenu.cs
@@ -0,0 +1,24 @@
+﻿using View.Entity;
+using View.Menu;
+
+namespace ShiftsLogger.Presentation.ShiftTypesMenu;
+
+public sealed class ShiftTypesMenu : MenuViewBase<Menu, MenuEntry>
+{
+    public ShiftTypesMenu(IServiceProvider serviceProvider) 
+        : base(serviceProvider)
+    {
+    }
+    protected override Menu CreateMenu()
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override IReadOnlyCollection<MenuEntry> GetMenuEntries()
+    {
+        throw new NotImplementedException();
+    }
+    
+    protected override TableSettings CreateSettings() =>
+        new MenuSettings();
+}
