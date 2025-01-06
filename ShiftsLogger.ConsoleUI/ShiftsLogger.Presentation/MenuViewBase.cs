5085cbf91c74ac33a2a11e13608a73b7294d4155
Eduard Fadieiev <eddy.fadeev@gmail.com>
2025-01-05 20:33:41 -0700

    feat(menu): add MenuViewBase class to handle menu views in ShiftsLogger.Presentation
    - Added MenuViewBase class with generic type parameters TMenu and TMenuEntry
    - Implemented abstract methods for creating menu, getting menu entries, and creating settings
    - Added BuildViewModel method to handle menu creation and initialization

---
+++ ShiftsLogger.ConsoleUI/ShiftsLogger.Presentation/MenuViewBase.cs
@@ -0,0 +1,52 @@
+﻿using Contracts;
+using Microsoft.Extensions.DependencyInjection;
+using Services.Contracts;
+using View.Contracts;
+using View.Entity;
+
+namespace ShiftsLogger.Presentation;
+
+public abstract class MenuViewBase<TMenu, TMenuEntry> : IMenu
+    where TMenu : ISelectable, IDisplayable
+{
+    private TMenu? _menuView;
+    
+    protected readonly IServiceProvider ServiceProvider;
+    protected readonly IMenuHandler MenuHandler;
+
+    private protected TableSettings? Settings { get; private set; }
+    private protected IReadOnlyCollection<TMenuEntry>? MenuEntries { get; private set; }
+
+    protected MenuViewBase(IServiceProvider serviceProvider)
+    {
+        ServiceProvider = serviceProvider;
+        MenuHandler = serviceProvider.GetRequiredService<IMenuHandler>();
+        
+        BuildViewModel();
+    }
+    
+    public ISelectable GetMenuView() =>
+        _menuView ?? 
+        throw new ArgumentNullException
+        (
+            paramName: nameof(_menuView), 
+            message: "Menu view is null."
+        );
+    
+    protected abstract TMenu CreateMenu();
+    protected abstract IReadOnlyCollection<TMenuEntry> GetMenuEntries();
+    protected abstract TableSettings CreateSettings();
+    
+    protected virtual void OnMenuCreation()
+    {
+        // Any logic that needs to be executed when the menu is created.
+    }
+    
+    private void BuildViewModel()
+    {
+        OnMenuCreation();
+        Settings = CreateSettings();
+        MenuEntries = GetMenuEntries();
+        _menuView = CreateMenu();
+    }
+}
