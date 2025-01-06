1fbb0da21a63e25c26734a9d4e0c0ecfd222e350
Eduard Fadieiev <eddy.fadeev@gmail.com>
2025-01-05 20:34:04 -0700

    feat(presentation): add ShiftsMenu class with Add Shift and Manage Shifts menu entries in ShiftsLogger
    - Created a new ShiftsMenu class in the Presentation folder with Add Shift and Manage Shifts menu entries
    - Implemented methods to handle Add Shift and Manage Shifts actions in the menu view

---
+++ ShiftsLogger.ConsoleUI/ShiftsLogger.Presentation/ShiftsMenu/ShiftsMenu.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using View.Entity;
+using View.Menu;
+using View.Services.Contracts;
+
+namespace ShiftsLogger.Presentation.ShiftsMenu;
+
+public sealed class ShiftsMenu : MenuViewBase<Menu, MenuEntry>
+{
+    public ShiftsMenu(IServiceProvider serviceProvider) 
+        : base(serviceProvider)
+    {
+    }
+
+    protected override Menu CreateMenu()
+    {
+        var tableBuilder = ServiceProvider.GetRequiredService<ITableBuilder>();
+        
+        ArgumentNullException.ThrowIfNull(tableBuilder, nameof(tableBuilder));
+        ArgumentNullException.ThrowIfNull(Settings, nameof(Settings));
+        ArgumentNullException.ThrowIfNull(MenuEntries, nameof(MenuEntries));
+        
+        return new Menu(tableBuilder, (MenuSettings)Settings, MenuEntries);
+    }
+
+    protected override IReadOnlyCollection<MenuEntry> GetMenuEntries() =>
+    [
+        new
+        (
+            entryText: "Add Shift",
+            entryAction: AddShift(),
+            entryStyle: Settings!.ContentStyle
+        ),
+        new
+        (
+            entryText: "Manage Shifts",
+            entryAction: ManageShifts(),
+            entryStyle: Settings!.ContentStyle
+        )
+    ];
+
+    protected override TableSettings CreateSettings() =>
+        new MenuSettings();
+
+    #region Actions
+
+    private Action AddShift() =>
+        () =>
+        {
+
+        };
+
+    private Action ManageShifts() =>
+        () =>
+        {
+
+        };
+
+    #endregion
+}
