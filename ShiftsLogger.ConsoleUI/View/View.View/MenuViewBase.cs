b6f6b5a0818a3fc53b3a098d8dd3a991e6877713
Eduard Fadieiev <eddy.fadeev@gmail.com>
2025-01-06 19:12:19 -0700

    refactor: Refactored MenuViewBase class. Added new functionality to the MenuViewBase class in the View namespace.

---
+++ ShiftsLogger.ConsoleUI/View/View.View/MenuViewBase.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Spectre.Console;
+using View.Contracts;
+using View.Entity.Models;
+using View.Entity.ViewModels;
+using View.Services.Contracts;
+
+namespace View.View;
+
+public abstract class MenuViewBase<TMenu, TMenuEntry> : IMenu
+    where TMenu : MenuViewModelBase<TMenuEntry>
+    where TMenuEntry : IActionable
+{
+    protected TMenu? MenuViewModel;
+    
+    protected readonly IServiceProvider ServiceProvider;
+    protected readonly IMenuHandler MenuHandler;
+
+    protected TableSettings? Settings;
+    protected IReadOnlyCollection<TMenuEntry>? MenuEntries;
+    protected ITableBuilderStrategy<TMenuEntry>? BuilderStrategy;
+
+    protected MenuViewBase(IServiceProvider serviceProvider)
+    {
+        ServiceProvider = serviceProvider;
+        MenuHandler = serviceProvider.GetRequiredService<IMenuHandler>();
+        
+        BuildViewModel();
+    }
+
+    public ISelectable GetViewModel() =>
+        MenuViewModel ?? 
+        throw new ArgumentNullException
+        (
+            paramName: nameof(MenuViewModel), 
+            message: "Menu view is null."
+        );
+
+    public virtual void DisplayMenu()
+    {
+        ArgumentNullException.ThrowIfNull(BuilderStrategy, nameof(BuilderStrategy));
+        ArgumentNullException.ThrowIfNull(MenuViewModel, nameof(MenuViewModel));
+        
+        var tableBuilder = ServiceProvider.GetRequiredService<ITableBuilder>();
+        var table = tableBuilder.Build(MenuViewModel.MenuData, BuilderStrategy);
+        
+        AnsiConsole.Clear();
+        AnsiConsole.Write(table);
+    }
+    
+    protected abstract TableSettings CreateSettings();
+    protected abstract IReadOnlyCollection<TMenuEntry> GetMenuEntries();
+    protected abstract TMenu CreateMenu();
+    protected abstract ITableBuilderStrategy<TMenuEntry> CreateStrategy();
+    
+    protected virtual void OnMenuCreation()
+    {
+        // Any logic that needs to be executed before the menu is created.
+    }
+
+    protected virtual void OnMenuCreated()
+    {
+        // Any logic that needs to be executed after the menu is created.
+    }
+    
+    private void BuildViewModel()
+    {
+        OnMenuCreation();
+        Settings = CreateSettings();
+        MenuEntries = GetMenuEntries();
+        MenuViewModel = CreateMenu();
+        BuilderStrategy = CreateStrategy();
+        OnMenuCreated();
+    }
+}
