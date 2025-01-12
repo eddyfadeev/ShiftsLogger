e9a0b3c0492387b46eeb3470280c8a175c4fbe05
Eduard Fadieiev <eddy.fadeev@gmail.com>
2025-01-11 21:40:52 -0700

    feat: Add MenuStyles class for customizing menu styles in the console UI.

---
+++ ShiftsLogger.ConsoleUI/View/View.Entity/Models/MenuStyles.cs
@@ -0,0 +1,11 @@
+﻿using Spectre.Console;
+
+namespace View.Entity.Models;
+
+public class MenuStyles
+{
+    public Style Title { get; set; } = new (foreground: Color.Olive);
+    public Style Footer { get; set; } = new(foreground: Color.White);
+    public Style Content { get; set; } = new(foreground: Color.White);
+    public Style Selection { get; set; } = new(foreground: Color.Gold3_1);
+}
