b4b09d0ddb56ac5f868a7540c14f2000e9cb05a0
Eduard Fadieiev <eddy.fadeev@gmail.com>
2025-01-11 21:41:39 -0700

    feat: refactor MenuHandler class to use INavigationHandler to handle key press events and navigation within menus.

--- ShiftsLogger.ConsoleUI/View/View.Services/MenuHandler.cs
+++ ShiftsLogger.ConsoleUI/View/View.Services/MenuHandler.cs
@@ -1,13 +1,17 @@
 ﻿using View.Contracts;
-using View.Entity.Structures;
 using View.Services.Contracts;
 
 namespace View.Services;
 
 public class MenuHandler : IMenuHandler
 {
+    private readonly INavigationHandler _navigationHandler;
+    
     private readonly Stack<IMenu> _menuStack = new();
     private bool _isRunning = true;
+
+    public MenuHandler(INavigationHandler navigationHandler) =>
+        _navigationHandler = navigationHandler;
     
     public async Task RunAsync()
     {
@@ -20,7 +24,7 @@ public class MenuHandler : IMenuHandler
             }
             
             currentMenu.DisplayMenu();
-            await HandleKeyPressAsync(currentMenu);
+            await _navigationHandler.HandleKeyPressAsync(selectableMenu: currentMenu.GetViewModel(), menuHandler: this);
         }
     }
 
@@ -38,47 +42,4 @@ public class MenuHandler : IMenuHandler
             _isRunning = false;
         }
     }
-    
-    private async Task HandleKeyPressAsync(IMenu menu)
-    {
-        if (!Console.KeyAvailable)
-        {
-            await Task.Delay(100);
-        }
-
-        var selectable = menu.GetViewModel();
-
-        var keyPress = Console.ReadKey(true);
-
-        switch (keyPress.Key)
-        {
-            case ConsoleKey.UpArrow:
-                selectable.SelectPrevious();
-                break;
-            case ConsoleKey.DownArrow:
-                selectable.SelectNext();
-                break;
-            case ConsoleKey.Enter:
-                selectable.InvokeAction();
-                break;
-            case ConsoleKey.Escape:
-                PopMenu();
-                break;
-            case ConsoleKey.LeftArrow
-                when menu is IPageable<EntityEntryData> pageable:
-                // TODO: Fetch data and pass it
-                pageable.PreviousPage();
-                break;
-            case ConsoleKey.RightArrow
-                when menu is IPageable<EntityEntryData> pageable:
-                // TODO: Fetch data and pass it
-                pageable.NextPage();
-                break;
-        }
-    }
-}
-
-public interface INavigationHandler
-{
-    Task HandleKeyPressAsync(ISelectable menu);
 }
