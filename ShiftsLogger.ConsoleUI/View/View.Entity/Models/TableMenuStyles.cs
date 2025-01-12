b8a05379a33376e3fbed71e6e63fcbfeeec300ee
Eduard Fadieiev <eddy.fadeev@gmail.com>
2025-01-11 21:40:56 -0700

    feat(view): add TableMenuStyles class with default properties for table border style and table header color styling.

---
+++ ShiftsLogger.ConsoleUI/View/View.Entity/Models/TableMenuStyles.cs
@@ -0,0 +1,9 @@
+﻿using Spectre.Console;
+
+namespace View.Entity.Models;
+
+public class TableMenuStyles : MenuStyles
+{
+    public TableBorder BorderStyle { get; set; } = TableBorder.Rounded;
+    public Style TableHeader { get; set; } = new(foreground: Color.White);
+}
