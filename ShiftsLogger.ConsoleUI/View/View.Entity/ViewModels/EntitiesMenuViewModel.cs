99a3af85a3f04dcd4d07dae8d34f59be5751fc7a
Eduard Fadieiev <eddy.fadeev@gmail.com>
2025-01-11 21:42:31 -0700

    refactor: Updated EntitiesMenuViewModel to implement IPageable interface and added event handler for page change. Fixed styling issues in MoveCursor method.

--- ShiftsLogger.ConsoleUI/View/View.Entity/ViewModels/EntitiesMenuViewModel.cs
+++ ShiftsLogger.ConsoleUI/View/View.Entity/ViewModels/EntitiesMenuViewModel.cs
@@ -2,13 +2,13 @@
 using View.Entity.Extensions;
 using View.Entity.Models;
 using View.Entity.Structures;
+using View.Events;
 
 namespace View.Entity.ViewModels;
 
-public class EntitiesMenuViewModel : MenuViewModelBase<EntityEntryData>, IPageable<EntityEntryData>
+public class EntitiesMenuViewModel : MenuViewModelBase<EntityEntryData>, IPageable
 {
     private int _currentPage = 1;
-    private int _totalPages = 1;
     
     public EntitiesMenuViewModel(
         TableSettings tableSettings, 
@@ -17,29 +17,38 @@ public class EntitiesMenuViewModel : MenuViewModelBase<EntityEntryData>, IPageab
     {
         MenuData[SelectedIndex] = 
             MenuData[SelectedIndex]
-                .WithStyle(MenuData.Settings.SelectionStyle);
+                .WithStyle(MenuData.Settings.Styles.Selection);
     }
 
     protected override void MoveCursor(int previousIndex, int newIndex)
     {
         MenuData[previousIndex] = 
             MenuData[previousIndex]
-                .WithStyle(MenuData.Settings.ContentStyle);
+                .WithStyle(MenuData.Settings.Styles.Content);
         
         MenuData[newIndex] = 
             MenuData[newIndex]
-                .WithStyle(MenuData.Settings.SelectionStyle);
+                .WithStyle(MenuData.Settings.Styles.Selection);
     }
 
-    private void SubstituteData(IEnumerable<EntityEntryData> data)
+    public void SubstituteData<T>(IEnumerable<T> data)
+        where T : IActionable
     {
         ArgumentNullException.ThrowIfNull(data, nameof(data));
+        var actionables = data.ToList();
+        
+        if (actionables.FirstOrDefault() is not EntityEntryData)
+        {
+            throw new ArgumentException("Invalid data type", nameof(data));
+        }
         MenuData.Clear();
-        MenuData.AddRange(data);
+        MenuData.AddRange((IEnumerable<EntityEntryData>)actionables);
     }
 
     #region IPageable
-    
+
+    public event ChangePageEvent? OnPageChanged;
+
     public int CurrentPage
     {
         get => _currentPage;
@@ -48,7 +57,7 @@ public class EntitiesMenuViewModel : MenuViewModelBase<EntityEntryData>, IPageab
             const int firstPage = 1;
             int previousPage = _currentPage;
             
-            int newPage = Math.Clamp(value, firstPage, TotalPages);
+            int newPage = Math.Clamp(value, firstPage, int.MaxValue);
             if (newPage == previousPage)
             {
                 return;
@@ -56,50 +65,34 @@ public class EntitiesMenuViewModel : MenuViewModelBase<EntityEntryData>, IPageab
 
             _currentPage = newPage;
             ResetSelection();
+            OnPageChanged?.Invoke(this, new ChangePageEventArgs { CurrentPage = newPage });
         } 
     }
 
-    public int TotalPages
-    {
-        get => _totalPages;
-        set
-        {
-            const int minPages = 1;
-            int previousTotalPages = _totalPages;
-            
-            int totalPages = Math.Clamp(value, minPages, int.MaxValue);
-            if (totalPages == previousTotalPages)
-            {
-                return;
-            }
-            
-            _totalPages = totalPages;
-        }
-    }
+    // public int TotalPages
+    // {
+    //     get => _totalPages;
+    //     set
+    //     {
+    //         const int minPages = 1;
+    //         int previousTotalPages = _totalPages;
+    //         
+    //         int totalPages = Math.Clamp(value, minPages, int.MaxValue);
+    //         if (totalPages == previousTotalPages)
+    //         {
+    //             return;
+    //         }
+    //         
+    //         _totalPages = totalPages;
+    //     }
+    // }
 
-    public void NextPage(IEnumerable<EntityEntryData> items)
-    {
-        SubstituteData(items);
+    public void NextPage() =>
         CurrentPage++;
-    }
+    
 
-    public void PreviousPage(IEnumerable<EntityEntryData> items)
-    {
-        SubstituteData(items);
+    public void PreviousPage() =>
         CurrentPage--;
-    }
-
-    public void SetCurrentPage(IEnumerable<EntityEntryData> items, int page)
-    {
-        SubstituteData(items);
-        CurrentPage = page;
-    }
-
-    public void ResetPage(IEnumerable<EntityEntryData> items)
-    {
-        SubstituteData(items);
-        CurrentPage = 1;
-    }
     
     #endregion
 }
