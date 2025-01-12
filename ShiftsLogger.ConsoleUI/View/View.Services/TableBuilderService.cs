04e2452b4fd840743968f6cedddb48103b37337f
Eduard Fadieiev <eddy.fadeev@gmail.com>
2025-01-11 21:42:04 -0700

    feat(view): adjust table width to 120 in TableBuilderService

--- ShiftsLogger.ConsoleUI/View/View.Services/TableBuilderService.cs
+++ ShiftsLogger.ConsoleUI/View/View.Services/TableBuilderService.cs
@@ -7,5 +7,5 @@ namespace View.Services;
 public class TableBuilderService : ITableBuilder
 {
     public Table Build<T>(TableData<T> tableData, ITableBuilderStrategy<T> strategy) =>
-        strategy.Build(tableData);
+        strategy.Build(tableData).Width(120);
 }
