83e0cf412dd9f2f9b8e589437137c8622a3dee39
Eduard Fadieiev <eddy.fadeev@gmail.com>
2025-01-12 19:25:28 -0700

    feat(presentation): add ManageShiftsMenu class for handling shifts management with title "Manage Shifts" and method to fetch entities from API.

---
+++ ShiftsLogger.ConsoleUI/ShiftsLogger.Presentation/ShiftsMenu/ManageShiftsMenu.cs
@@ -0,0 +1,19 @@
+﻿using Shared.Dto.Shift;
+using Shared.RequestFeatures;
+
+namespace ShiftsLogger.Presentation.ShiftsMenu;
+
+public sealed class ManageShiftsMenu : ManageEntitiesMenu<ShiftDto>
+{
+    private const string Title = "Manage Shifts";
+    
+    public ManageShiftsMenu(IServiceProvider serviceProvider) 
+        : base(serviceProvider, Title)
+    {
+    }
+
+    private protected override async Task<PagedList<ShiftDto>> 
+        FetchEntities(RequestParameters? requestParameters = null) => 
+        await ApiService.Shift.GetAllShiftsAsync(requestParameters as ShiftParameters);
+            
+}
